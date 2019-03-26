57a5bac3d7c7d3493c11dfb4fb8fe33f92665a9e
Felipe Lalanne <felipe@lalanne.cl>
2019-03-26 18:48:18 -0300

    Update main to use event API

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /**
  * Basic tcp server using select()
  * Based on https://www.gnu.org/software/libc/manual/html_node/Server-Example.html
- * 
+ *
  * @author Felipe Lalanne <flalanne@niclabs.cl>
  */
 
@@ -10,24 +10,163 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <sys/select.h>
+
+#include "event_loop.h"
 
 #define PORT (8888)
 #define MAX_BUF_SIZE (128)
-#define MAX_CLIENTS (1)
+#define MAX_NO_OF_CLIENTS (1)
 
-int make_socket(uint16_t port)
+typedef struct {
+    int sock;
+    int is_used;
+    event_handler_t handler;
+} client_t;
+
+typedef struct {
+    int sock;
+    event_handler_t handler;
+
+    client_t clients[MAX_NO_OF_CLIENTS];
+} server_t;
+
+static server_t server;
+
+
+static int find_client(server_t *server, client_t *client)
+{
+    int client_slot = -1;
+
+    for (int i = 0; i < MAX_NO_OF_CLIENTS && client_slot < 0; i++) {
+        if (client->is_used == 1 && client == &server->clients[i]) {
+            client_slot = i;
+        }
+    }
+
+    return client_slot;
+}
+
+static int find_free_slot(server_t *server)
+{
+    int slot = -1;
+    for (int i = 0; i < MAX_NO_OF_CLIENTS && slot < 0; i++) {
+        if (server->clients[i].is_used == 0) {
+            slot = i;
+        }
+    }
+    return slot;
+}
+
+int get_client_socket(void *instance)
+{
+    const client_t *client = instance;
+
+    return client->sock;
+}
+
+int get_server_socket(void *instance)
+{
+    const server_t *server = instance;
+
+    return server->sock;
+}
+
+void on_recv(void *instance)
+{
+    client_t *client = instance;
+    int fd = client->sock;
+
+    char buffer[MAX_BUF_SIZE];
+    int nbytes;
+
+    nbytes = read(fd, buffer, MAX_BUF_SIZE);
+    if (nbytes < 0) {
+        /* Read error. */
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+    else if (nbytes == 0) {
+        // close socket and unregister
+        server_t *s = &server;
+        int slot = find_client(s, client);
+
+        if (slot >= 0) {
+            s->clients[slot].is_used = 0;
+            unregister_handler(&client->handler);
+        }
+        return;
+    }
+    else {
+        /* Data read. */
+        fprintf(stderr, "Server: got message: '%.*s'\n", nbytes - 1, buffer);
+    }
+}
+
+void create_client(int sock, client_t *client)
+{
+    event_handler_t h = { .instance = client, .get_fd = get_client_socket, .handle = on_recv };
+    client->sock = sock;
+    client->handler = h;
+    client->is_used = 1;
+}
+
+void on_new_client(void *instance)
+{
+    server_t *server = instance;
+    event_handler_t *handler = &server->handler;
+
+    assert(handler != NULL);
+
+    // Setup address
+    char addr_str[INET6_ADDRSTRLEN];
+    struct sockaddr_in6 addr;
+    socklen_t addr_size = sizeof(addr);
+
+    int new = accept(handler->get_fd(server),
+                     (struct sockaddr *)&addr,
+                     &addr_size);
+    if (new < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    inet_ntop(AF_INET6, &addr.sin6_addr, addr_str, sizeof(addr_str));
+    fprintf(stderr,
+            "Server: connect from host %s, port %u. ",
+            addr_str,
+            addr.sin6_port);
+
+    // Check if number of client connections has been reached
+    int slot = find_free_slot(server);
+    if (slot >= 0) {
+        client_t *client = &server->clients[slot];
+        create_client(new, client);
+
+        // Register the client handler
+        register_handler(&client->handler);
+
+
+        fprintf(stderr, "Accepted\n");
+    }
+    else {
+        // Reject new connection
+        close(new);
+        fprintf(stderr, "Rejected\n");
+    }
+}
+
+server_t *create_server(uint16_t port)
 {
     int sock = -1, option = 1;
     struct sockaddr_in6 addr;
 
     /* Create the socket. */
     sock = socket(AF_INET6, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
+    if (sock < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -37,129 +176,45 @@ int make_socket(uint16_t port)
     addr.sin6_port = htons(port);
     memset(&addr.sin6_addr, 0, sizeof(addr.sin6_addr));
 
-    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
     /* avoid waiting port close time to use server immediately */
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&option, sizeof(option)) < 0)
-    {
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&option, sizeof(option)) < 0) {
         perror("setsockopt");
         close(sock);
         exit(EXIT_FAILURE);
     }
 
-    return sock;
+    server_t *s = &server;
+    event_handler_t h = { .instance = s, .get_fd = get_server_socket, .handle = on_new_client };
+    s->sock = sock;
+    s->handler = h;
+
+    return s;
 }
 
-int read_from_client(int fd)
+void server_listen(server_t *server)
 {
-    char buffer[MAX_BUF_SIZE];
-    int nbytes;
-
-    nbytes = read(fd, buffer, MAX_BUF_SIZE);
-    if (nbytes < 0)
-    {
-        /* Read error. */
-        perror("read");
+    if (listen(server->sock, MAX_NO_OF_CLIENTS) < 0) {
+        perror("listen");
         exit(EXIT_FAILURE);
-    }
-    else if (nbytes == 0)
-        /* End-of-file. */
-        return -1;
-    else
-    {
-        /* Data read. */
-        fprintf(stderr, "Server: got message: '%.*s'\n", nbytes - 1, buffer);
-        return 0;
     }
 }
 
 int main(void)
 {
-    int sock;
-    fd_set active_fd_set, read_fd_set;
-    int i;
-    char client_addr_str[INET6_ADDRSTRLEN];
-    struct sockaddr_in6 client_addr;
-    socklen_t size;
-    int clients = 0;
-
     /* Create the socket and set it up to accept connections. */
-    sock = make_socket(PORT);
-    if (listen(sock, MAX_CLIENTS) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    server_t *s = create_server(PORT);
 
-    /* Initialize the set of active sockets. */
-    FD_ZERO(&active_fd_set);
-    FD_SET(sock, &active_fd_set);
+    // Register server handler
+    register_handler(&s->handler);
 
-    while (1)
-    {
-        // TODO: move loop outside main()
+    // Start listening for events
+    server_listen(s);
 
-        /* Block until input arrives on one or more active sockets. */
-        read_fd_set = active_fd_set;
-        if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
-        {
-            perror("select");
-            exit(EXIT_FAILURE);
-        }
-
-        /* Service all the sockets with input pending. */
-        for (i = 0; i < FD_SETSIZE; ++i)
-            if (FD_ISSET(i, &read_fd_set))
-            {
-                if (i == sock)
-                {
-                    /* Connection request on original socket. */
-                    int new;
-                    size = sizeof(client_addr);
-                    new = accept(sock,
-                                 (struct sockaddr *)&client_addr,
-                                 &size);
-                    if (new < 0)
-                    {
-                        perror("accept");
-                        exit(EXIT_FAILURE);
-                    };
-
-                    inet_ntop(AF_INET6, &client_addr.sin6_addr, client_addr_str, sizeof(client_addr_str));
-                    fprintf(stderr,
-                            "Server: connect from host %s, port %u. ",
-                            client_addr_str,
-                            client_addr.sin6_port);
-                    if (clients < MAX_CLIENTS)
-                    {
-                        FD_SET(new, &active_fd_set);
-                        clients++;
-                        fprintf(stderr, "Acepted\n");
-
-                        // TODO: call handler to notify of new connection
-                    }
-                    else
-                    {
-                        // Reject new connection
-                        close(new);
-                        fprintf(stderr, "Rejected\n");
-                    }
-                }
-                else
-                {
-                    // TODO: call handler to notify of pending data
-                    /* Data arriving on an already-connected socket. */
-                    if (read_from_client(i) < 0)
-                    {
-                        close(i);
-                        FD_CLR(i, &active_fd_set);
-                        clients--;
-                    }
-                }
-            }
-    }
+    // Start event-loop
+    handle_events();
 }
