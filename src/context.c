16fff4b420b631130a23e3edc788e3ce02f0be9b
Felipe Lalanne <felipe@lalanne.cl>
2019-03-27 18:15:33 -0300

    Define client context operations

---
+++ src/context.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "context.h"
+
+void client_ctx_init(int fd, client_ctx_t * ctx) {
+    assert(ctx != NULL);
+
+    ctx->fd = fd;
+
+    // TODO: perform http2 state initialization
+}
+
+void client_ctx_destroy(client_ctx_t * ctx) {
+    assert(ctx != NULL);
+
+    // TODO: perform closeup tasks
+    close(ctx->fd);
+}
