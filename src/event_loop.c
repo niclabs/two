a0b58eef28983dd966e0ca698105542436e7933b
Felipe Lalanne <felipe@lalanne.cl>
2019-03-27 12:04:56 -0300

    Use pointer in handler list to reduce memory usage

--- src/event_loop.c
+++ src/event_loop.c
@@ -30,7 +30,7 @@ static int active_fd_set_is_init = 0;
 typedef struct {
     int fd;
     int is_used;
-    event_handler_t event;
+    event_handler_t *event;
 } handler_list_entry_t;
 
 // List of registered handlers
@@ -42,7 +42,7 @@ event_handler_t *find_handler(int fd)
 
     for (int i = 0; i < MAX_NO_OF_HANDLES && match == NULL; i++) {
         if (registered_handlers_list[i].is_used && registered_handlers_list[i].fd == fd) {
-            match = &registered_handlers_list[i].event;
+            match = registered_handlers_list[i].event;
         }
     }
     return match;
@@ -75,7 +75,7 @@ int register_handler(event_handler_t *event)
         int free_slot = find_free_handler();
         if (free_slot >= 0) {
             handler_list_entry_t *free_entry = &registered_handlers_list[free_slot];
-            free_entry->event = *event;
+            free_entry->event = event;
             free_entry->fd = fd;
             free_entry->is_used = 1;
 
