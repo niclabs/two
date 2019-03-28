e53b514866d9385500ba645424107e9939679ffd
Felipe Lalanne <felipe@lalanne.cl>
2019-03-27 23:50:30 -0300

    Reduce select range from FD_SETSIZE to max fd + 1

--- src/event_loop.c
+++ src/event_loop.c
@@ -19,6 +19,7 @@
 // Active file descriptors
 static fd_set active_fd_set;
 static int active_fd_set_is_init = 0;
+static int max_fd = -1;
 
 /**
  * List item definition for a list of
@@ -77,6 +78,9 @@ int register_handler(event_handler_t *event)
             free_entry->is_used = 1;
 
             FD_SET(fd, &active_fd_set);
+            if (fd > max_fd) {
+                max_fd = fd;
+            }
 
             return 0;
         }
@@ -102,7 +106,10 @@ int unregister_handler(event_handler_t *event)
             if (registered_handlers_list[i].is_used && registered_handlers_list[i].fd == fd) {
                 registered_handlers_list[i].is_used = 0;
                 node_removed = 0;
+
+                // Clear the file descriptor and find the new max_fd
                 FD_CLR(fd, &active_fd_set);
+                while (FD_ISSET(max_fd, &active_fd_set)) max_fd--;
             }
         }
     }
@@ -116,13 +123,13 @@ void handle_events(void)
     for (;;) {
         /* Block until input arrives on one or more active sockets. */
         read_fd_set = active_fd_set;
-        if (select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
+        if (select(max_fd + 1, &read_fd_set, NULL, NULL, NULL) < 0) {
             ERROR("In select(): %s\n", strerror(errno));
             break;
         }
 
         /* Service all the sockets with input pending. */
-        for (int i = 0; i < FD_SETSIZE; ++i) {
+        for (int i = 0; i <= max_fd; ++i) {
             if (FD_ISSET(i, &read_fd_set)) {
                 event_handler_t *signalled_event = find_handler(i);
                 if (signalled_event != NULL) {
