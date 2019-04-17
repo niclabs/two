f2851f17563487de90bf1cdfdbb59b27720803f3
Kelly Wagemann <kelly.andrea97@gmail.com>
2019-04-16 20:34:50 -0400

    Done: Specify different types of error.

--- src/sock.c
+++ src/sock.c
@@ -18,7 +18,8 @@
 int sock_create(sock_t * sock) {
     sock->fd=socket(AF_INET6, SOCK_STREAM, 0);  
     if(sock->fd <0){
-	    return -1; //TODO specify different types of error.
+        perror("Error creating socket");
+	    return -1; 
     }
 	sock->state = SOCK_OPENED;
 	return 0;
@@ -31,10 +32,12 @@ int sock_listen(sock_t * server, uint16_t port) {
     sin6.sin6_addr=in6addr_any; 
     assert(server->state == SOCK_OPENED);
     if(bind(server->fd, (struct sockaddr *)&sin6, sizeof(sin6))<0){
+        perror("Error on binding");
         return -1;
     }
     else if (listen(server->fd, BACKLOAD)<0){
-       return -1;
+        perror("Error on listening");
+        return -1;
     }
 	server->state= SOCK_LISTENING;
     return 0;
@@ -44,7 +47,8 @@ int sock_accept(sock_t * server, sock_t * client) {
     assert(server->state == SOCK_LISTENING);
     int clifd=accept(server->fd, NULL, NULL);
     if(clifd){
-	    return -1; //TODO specify different types of error.
+        perror("Error on accept");
+	    return -1; 
     }
 	client->fd=clifd;
 	server->state=SOCK_CONNECTED; 
@@ -61,7 +65,8 @@ int sock_connect(sock_t * client, char * addr, uint16_t port) {
     sin6.sin6_addr=address;
     assert(client->state == SOCK_OPENED);
     if(connect(client->fd, (struct sockaddr*)&sin6, sizeof(sin6))<0){
-	    return -1; //TODO specify different types of error.
+        perror("Error on connect");
+	    return -1; 
     }
     client->state=SOCK_CONNECTED;
 	return 0;
@@ -72,6 +77,7 @@ int sock_read(sock_t * sock, char * buf, int len, int timeout) {
     (void)timeout;
    	bzero(buf, BUF_LEN);
    	if((read(sock->fd, buf, len))<0){
+        perror("Error reading from socket");
         return -1;
  	} //message stored in buf.
     return 0;
@@ -83,7 +89,8 @@ int sock_write(sock_t * sock, char * buf, int len) {
    	fgets(buf,len,stdin); //store message in buf.
     //from where it is received? FILE *stream?
    	if ((write(sock->fd, buf, len)<0)){
-     	   return -1;
+        perror("Error writing on socket");
+     	return -1;
     }
     // message (len bytes) written from buf to sock.
     return 0;
@@ -92,7 +99,8 @@ int sock_write(sock_t * sock, char * buf, int len) {
 int sock_destroy(sock_t * sock) {
     assert(sock->state != SOCK_CLOSED);
     if(close(sock->fd)<0){
-	    return -1;//TODO specify different types of error.
+        perror("Error destroying socket");
+	    return -1;
     }
 	sock->state=SOCK_CLOSED;
 	return 0;
