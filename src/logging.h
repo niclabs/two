b8f50edcdb0953f7d62866758b1e39e00e49c530
Felipe Lalanne <flalanne@niclabs.cl>
2019-04-18 11:40:28 -0400

    Simplify LOG macro

--- src/logging.h
+++ src/logging.h
@@ -1,13 +1,13 @@
 /**
  * Logging and debugging API
- * 
+ *
  * Defines the DEBUG() INFO() WARN() and ERROR() macros that will print
  * a message depending on the level defined by the variable LEVEL (default is INFO)
- * 
+ *
  * When debugging, it might be useful to set level to DEBUG, either by setting the variable
  * ENABLE_DEBUG to 1, by using the -DENABLE_DEBUG=1 on compilation, or by directly setting the LEVEL
  * variable to DEBUG, by using -DLEVEL=DEBUG
- * 
+ *
  * @author Felipe Lalanne <flalanne@niclabs.cl>
  */
 #ifndef LOGGING_H
@@ -46,32 +46,28 @@ typedef enum
 
 #define LEVEL_STR(level) #level
 
-        if (level >= LEVEL)                                                                                      \
-        {                                                                                                        \
-            char msg[256];                                                                                       \
-            snprintf(msg, LOGGING_MAX_MSG_LEN, fmt, ##__VA_ARGS__);                                              \
-            switch (level)                                                                                       \
-            {                                                                                                    \
-            case DEBUG:                                                                                          \
-                fprintf(stderr, "[DEBUG] %s: %s() in %s:%d\n", msg, func, file, line);                           \
-                break;                                                                                           \
-            case INFO:                                                                                           \
-                fprintf(stderr, "[INFO] %s: %s() in %s:%d\n", msg, func, file, line);                            \
-                break;                                                                                           \
-            case WARN:                                                                                           \
-                fprintf(stderr, "[WARN] %s: %s() in %s:%d\n", msg, func, file, line);                            \
-                break;                                                                                           \
-            case ERROR:                                                                                          \
-                if (errno > 0)                                                                                   \
-                    fprintf(stderr, "[ERROR] %s (%s): %s() in %s:%d\n", msg, strerror(errno), func, file, line); \
-                else                                                                                             \
-                    fprintf(stderr, "[ERROR] %s: %s() in %s:%d\n", msg, func, file, line);                       \
-                if (LOGGING_EXIT_ON_ERROR)                                                                       \
-                    exit(EXIT_FAILURE);                                                                          \
-                break;                                                                                           \
-            }                                                                                                    \
-        }                                                                                                        \
-    } while (0);
+#define LOG(level, func, file, line, fmt, ...)                                                                       \
+    do                                                                                                               \
+    {                                                                                                                \
+        if (level >= LEVEL)                                                                                          \
+        {                                                                                                            \
+            switch (level)                                                                                           \
+            {                                                                                                        \
+            case DEBUG:                                                                                              \
+            case INFO:                                                                                               \
+            case WARN:                                                                                               \
+                fprintf(stderr, "%s() in %s:%d [" LEVEL_STR(level) "]: " fmt "\n", func, file, line, ##__VA_ARGS__); \
+            case ERROR:                                                                                              \
+                if (errno > 0)                                                                                       \
+                {                                                                                                    \
+                    fprintf(stderr, "(%s)", strerror(errno));                                                        \
+                }                                                                                                    \
+                break;                                                                                               \
+            case FATAL:                                                                                              \
+                exit(EXIT_FAILURE);                                                                                  \
+            }                                                                                                        \
+        }                                                                                                            \
+    } while (0)
 
 // Macro to print debugging messages
 #define DEBUG(...) LOG(DEBUG, __func__, __FILE__, __LINE__, __VA_ARGS__)
