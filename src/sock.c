a6419ae9adaafa7ae7c38beb7eeb87d3109e6434
Kelly Wagemann <kelly.andrea97@gmail.com>
2019-04-24 13:17:24 -0400

    Fixed if statement in sock_accept function.

--- src/sock.c
+++ src/sock.c
@@ -45,7 +45,7 @@ int sock_listen(sock_t * server, uint16_t port) {
 int sock_accept(sock_t * server, sock_t * client) {
     int clifd=accept(server->fd, NULL, NULL);
     ASSERT(server->state == SOCK_LISTENING);
-    if(clifd){
+    if(clifd<0){
         perror("Error on accept");
 	    return -1; 
     }
