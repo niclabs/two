59e614efb95ae1180b5318d08f8f71cfc7f34a5f
[Javi] <[javiera.alegria@live.cl]>
2019-04-24 11:55:05 -0400

    API HTTP methods complete and main methods implemented

--- src/http_methods.c
+++ src/http_methods.c
@@ -3,48 +3,130 @@ This API contains the HTTP methods to be used by
 HTTP/2
 */
 
-//TODO: establecer variables globales
+#include <netinet/in.h>
 
-/*******************************Server*******************************/
+#include "http_methods.h"
+#include "sock.h"
 
-/*
-Given a port number this function initialize a server
-Input: port number
-Output: 0 if the action was successful, -1 if not
-*/
-int init_server(uint16_t port){
-  /*iniciar con sock_create, luego
-  abrir listen infinito con sock_listen*/
+
+struct client_s{
+  enum client_state {
+      EMPTY_CLIENT,
+      CREATED,
+      CONNECTED
+  } state;
+  sock_t * socket;
+};
+
+struct server_s{
+  enum server_state {
+    EMPTY_SERVER,
+    LISTEN
+  } state;
+  sock_t * socket;
+  struct client_s client_connect;
+};
+
+struct server_s server;
+struct client_s client;
+
+/************************************Server************************************/
+
+int http_init_server(uint16_t port){
+  struct server_s * sr= &server;
+  struct client_s * empty_client= &client;
+  empty_client->state=EMPTY_CLIENT;
+
+  sock_t * server_sock;
+
+  if (sock_create(server_sock)<0){
+    puts("Server could not be created");
+    return -1;
+  }
+
+  if (sock_listen(server_sock, port)<0){
+    puts("Partial error in server creation");
+    return -1;
+  }
+
+  sr->state=CREATED;
+  sr->socket=server_sock;
+
+  sock_t * client_sock;
+
+  if (sock_accept(sr->socket, client_sock)<0){
+    puts("Not client found");
+    return 0;
+  }
+
+  struct client_s * cl= &server.client_connect;
+  cl->state=CONNECTED;
+  cl->socket=client_sock;
+
+  if(server_init_connection()<0){
+    puts("");
+  }
+
+  return 0;
+}
+
+int http_set_function_to_path(char * callback, char * path){
+  //TODO callback(argc,argv)
   return -1;
 }
 
-/*
-Set an internal server function to a specific path
-Input: function name
-       specific path
-Output: 0 if the action was successful, -1 if not
-*/
-int set_function_to_path(char * fun_name, char * path){
-  /*manejar errores*/
-  return -1
+/************************************Client************************************/
+
+
+int http_client_connect(uint16_t port, char * ip){
+  struct client_s * cl= &client;
+  struct server_s * sr= &server;
+  sr->state=EMPTY_CLIENT;
+
+  sock_t * sock;
+
+  if (sock_create(sock)<0){
+    puts("Client could not be created");
+    return -1;
+  }
+
+  cl->socket=sock;
+  cl->state=CREATED;
+
+  if (sock_connect(sock, ip, port)<0){
+    puts("Client could not be connected");
+    return -1;
+  }
+
+  cl->state=CONNECTED;
+
+  if(client_init_connection()<0){
+    puts("");
+  }
+
+  return 0;
 }
 
-/*
-Given the content of the request made by the client, this function calls
-the functions necessary to respond to the request
-Input: Encoded request
-Output: 0 if the action was successful, -1 if not
-*/
-int recieve(char * headers){
+
+int http_client_disconnect(void){
+
+  if (client.state==CREATED){
+    return 0;
+  }
+
+  if (sock_destroy(client.socket)<0){
+    puts("Client could not be disconnected");
+    return -1;
+  }
+
+  return 0;
+}
+
+int http_receive(char * headers){
+  //TODO decod headers
   return -1;
 }
 
-/*
-
-Input: path where the function is
-
-Output: 0 if the action was successful, -1 if not
-*/
 int get_receive(char * path, char * headers){
   // buscar respuesta correspondiente a path
   // codificar respuesta
@@ -52,52 +134,61 @@ int get_receive(char * path, char * headers){
   return -1;
 }
 
-/*
-This function write in the socket with the client
-Input: buffer with the data to writte
-Output: 0 if the action was successful, -1 if not
-*/
+/******************************************************************************/
+
+
 int http_write(char * buf, int len){
-  /*sock_write*/
-  return -1;
+  if (client.state==EMPTY_CLIENT && server.state==EMPTY_CLIENT){
+    puts("Could not write");
+    return -1;
+  }
+
+  sock_t * sock;
+
+  if (client.state==EMPTY_CLIENT && server.state==CREATED){
+    sock = server.socket;
+  }
+  if (server.state==EMPTY_CLIENT){
+    if (client.state != CONNECTED){
+      puts("Client not connected");
+      return -1;
+    }
+    sock = client.socket;
+  }
+
+  int wr= sock_write(sock, buf, len);
+  if (wr<=0){
+    puts("Could not write");
+    return wr;
+  }
+  return wr;
 }
 
-/*
-This function read the data from the socket with the client
-Input: buffer where the data will be write
-Output: 0 if the action was successful, -1 if not
-*/
+
 int http_read(char * buf, int len){
-  /*sock_read*/
-  return -1;
-}
+  if (client.state==EMPTY_CLIENT && server.state==EMPTY_CLIENT){
+    puts("Could not read");
+    return -1;
+  }
 
+  sock_t * sock;
 
-/*******************************Client*******************************/
+  if (client.state==EMPTY_CLIENT && server.state==CREATED){
+    sock = server.socket;
+  }
+  if (server.state==EMPTY_CLIENT){
+    if (client.state != CONNECTED){
+      puts("Client not connected");
+      return -1;
+    }
+    sock = client.socket;
+  }
 
-/*
-This function start a connection from client to server
-Intput: port number
-        ip adress
-Output: 0 if the connection was successful,-1 if not
-*/
-int client_connect(uint16_t port, char * ip){
-  return -1;
-}
+  int rd =  sock_read(sock, buf, len, 0);
+  if (rd<=0){
+    puts("Could not read");
+    return rd;
+  }
+  return rd;
 
-/*
-This function stop the connection between client and server
-Output: 0 if the action was successful, -1 if not
-*/
-int client_disconnect(void){
-  return -1;
-}
-
-/*
-
-Input:
-Output:
-*/
-int get(char * path, char * headers){
-  return -1/*funcion de capa inferior*/;
 }
--- src/http_methods.h
+++ src/http_methods.h
@@ -3,20 +3,83 @@ This API contains the HTTP methods to be used by
 HTTP/2
 */
 
+#include <stdlib.h>
+
 /*Server*/
-int init_server(uint16_t port);
+/*
+* Given a port number this function initialize a server
+*
+* @param    port    port number
+*
+* @return   0       Server was successfully initialized
+* @return   -1      Server wasn't initialized
+*/
+int http_init_server(uint16_t port);
 
-int set_function_to_path(char * fun_name, char * path);
+/*
+* Set an internal server function to a specific path
+*
+* @param    fun_name   function name
+* @param    path       specific path
+*
+* @return   0          the action was successful
+* @return   -1         the action fail
+*/
+int http_set_function_to_path(char * fun_name, char * path);
 
-int recieve(char * headers);
-
+/*
+* Write in the socket with the client
+*
+* @param   buf   buffer with the data to writte
+* @param   len   buffer length
+*
+* @return >0   number of bytes written
+* @return 0    if connection was closed on the other side
+* @return -1   on error
+*/
 int http_write(char * buf, int len);
 
+/*
+* Read the data from the socket with the client
+*
+* @param    buf   buffer where the data will be write
+* @param    len   buffer length
+*
+* @return   >0     number of bytes read
+* @return   0      if connection was closed on the other side
+* @return  -1      on error
+*/
 int http_read(char * buf, int len);
 
+
 /*Client*/
-int client_connect(uint16_t port, char * ip);
 
-int client_disconnect(void);
+/*
+* Start a connection from client to server
+*
+* @param    port   port number
+* @param    ip     adress
+*
+* @return   0      successfully started connection
+* @return   -1     the connection fail
+*/
+int http_client_connect(uint16_t port, char * ip);
 
-int get(char * path, char * headers);
+/*
+* Stop the connection between client and server
+*
+* @return    0    connection was stopped
+* @return    -1   failed to stop connection
+*/
+int http_client_disconnect(void);
+
+/*
+* Given the content of the request made by the client, this function calls
+* the functions necessary to respond to the request
+*
+* @param    headers   Encoded request
+*
+* @return   0         the action was successful
+* @return   -1        the action fail
+*/
+int http_receive(char * headers);
