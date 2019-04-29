0fe7c583d23adc78f05faf04b4a55f5f73a6cc0b
Kelly W <kelly.andrea97@gmail.com>
2019-04-29 14:53:54 -0400

    Changed assertions with if statements.

--- src/sock.c
+++ src/sock.c
@@ -17,6 +17,7 @@
 int sock_create(sock_t * sock) {
     sock->fd=socket(AF_INET6, SOCK_STREAM, 0);  
     if(sock->fd <0){
+        errno=11;
         perror("Error creating socket");
 	    return -1; 
     }
@@ -25,7 +26,11 @@ int sock_create(sock_t * sock) {
 }
 
 int sock_listen(sock_t * server, uint16_t port) {
-    ASSERT(server->state == SOCK_OPENED);
+    if(server->state != SOCK_OPENED){
+        errno=22;
+        printf("Error in sock_listen, %s, server state must be opened.\n", strerror(errno));
+        return -1;
+    }
     struct sockaddr_in6 sin6;
     sin6.sin6_family=AF_INET6;
     sin6.sin6_port=htons(port);
@@ -43,7 +48,11 @@ int sock_listen(sock_t * server, uint16_t port) {
 }
 
 int sock_accept(sock_t * server, sock_t * client) {
-    ASSERT(server->state == SOCK_LISTENING);
+    if(server->state != SOCK_LISTENING){
+        errno=22;
+        printf("Error in sock_accept, %s, server state must be listening.\n", strerror(errno));
+        return -1;
+    }
     int clifd=accept(server->fd, NULL, NULL);
     if(clifd<0){
         perror("Error on accept");
@@ -60,17 +69,23 @@ int sock_accept(sock_t * server, sock_t * client) {
 }
 
 int sock_connect(sock_t * client, char * addr, uint16_t port) {
-    ASSERT(client->state == SOCK_OPENED);
+    if(client->state != SOCK_OPENED){
+        errno=22;
+        printf("Error in sock_connect, %s, client state must be opened.\n", strerror(errno));
+        return -1;
+    }
     struct sockaddr_in6 sin6;
     struct in6_addr address;
     if(addr==NULL){
-        printf("Error on connect: address given must not be NULL");
+        errno=14;
+        printf("Error in sock_connect: %s. Address given must not be NULL.\n", strerror(errno));
         return -1;
     }
     int inet_return= inet_pton(AF_INET6, addr, &address);
     if(inet_return<1){
         if(inet_return==0){
-            printf("Error converting IPv6 address to binary: your address does not contain a character string representing a valid network address in the specified family.");
+            errno=14;
+            perror("Error in sock_connect converting IPv6 address to binary");
         }
         if(inet_return==-1){
             perror("Error converting IPv6 address to binary");
@@ -89,7 +104,11 @@ int sock_connect(sock_t * client, char * addr, uint16_t port) {
 }
 
 int sock_read(sock_t * sock, char * buf, int len, int timeout) {
-    ASSERT(sock->state == SOCK_CONNECTED);
+    if(sock->state != SOCK_CONNECTED){
+        errno=22;
+        printf("Error in sock_read, %s, sock state must be connected.\n", strerror(errno));
+        return -1;
+    }
     struct timeval time_o;
     char *p = buf;
     int time_taken=0;
@@ -100,7 +119,8 @@ int sock_read(sock_t * sock, char * buf, int len, int timeout) {
         timeout=timeout-time_taken;
         time_o.tv_sec = timeout;
         if(timeout<0){
-            printf("Timeout exceeded in sock_read function.");
+            errno=62;
+            printf("%s in sock_read function.\n", strerror(errno));
             return -1;
         }
         if (setsockopt(sock->fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&time_o, sizeof(time_o)) < 0) {
@@ -122,12 +142,17 @@ int sock_read(sock_t * sock, char * buf, int len, int timeout) {
     time_o.tv_usec = 0;
     if (setsockopt(sock->fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&time_o, sizeof(time_o)) < 0) {
         perror("Error unsetting timeout");
+        return -1;
     }
     return 0;
 }
 
 int sock_write(sock_t * sock, char * buf, int len) {
-    ASSERT(sock->state == SOCK_CONNECTED);
+    if(sock->state == SOCK_CONNECTED){
+        errno=22;
+        printf("Error in sock_write, %s, sock state must be connected.\n", strerror(errno));
+        return -1;
+    }
     ssize_t n;
     const char *p = buf;
     while(len>0){
@@ -143,7 +168,11 @@ int sock_write(sock_t * sock, char * buf, int len) {
 }
 
 int sock_destroy(sock_t * sock) {
-    ASSERT(sock->state != SOCK_CLOSED);
+    if(sock->state == SOCK_CLOSED){
+        errno=114;
+        perror("Error on sock_destroy");
+        return -1;
+    }
     if(close(sock->fd)<0){
         perror("Error destroying socket");
 	    return -1;
