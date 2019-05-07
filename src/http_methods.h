4672157a8a88a3c9285dd3bad461ec37bb20ed3f
[Javi] <[javiera.alegria@live.cl]>
2019-05-07 12:39:52 -0400

    Fixed API http methods

--- src/http_methods.h
+++ src/http_methods.h
@@ -17,6 +17,39 @@ HTTP/2
 int http_init_server(uint16_t port);
 
 /*
+* Set an internal server function to a specific path
+*
+* @param    fun_name   function name
+* @param    path       specific path
+*
+* @return   0          the action was successful
+* @return   -1         the action fail
+*/
+int http_set_function_to_path(char * fun_name, char * path);
+
+
+/*Client*/
+
+/*
+* Start a connection from client to server
+*
+* @param    port   port number
+* @param    ip     adress
+*
+* @return   0      successfully started connection
+* @return   -1     the connection fail
+*/
+int http_client_connect(uint16_t port, char * ip);
+
+/*
+* Stop the connection between client and server
+*
+* @return    0    connection was stopped
+* @return    -1   failed to stop connection
+*/
+int http_client_disconnect(void);
+
+/*
 * Write in the socket with the client
 *
 * @param   buf   buffer with the data to writte
@@ -40,24 +73,13 @@ int http_write(uint8_t * buf, int len);
 */
 int http_read(uint8_t * buf, int len);
 
-
-/*Client*/
-
 /*
-* Start a connection from client to server
+* Given the content of the request made by the client, this function calls
+* the functions necessary to respond to the request
 *
-* @param    port   port number
-* @param    ip     adress
+* @param    headers   Encoded request
 *
-* @return   0      successfully started connection
-* @return   -1     the connection fail
+* @return   0         the action was successful
+* @return   -1        the action fail
 */
-int http_client_connect(uint16_t port, char * ip);
-
-/*
-* Stop the connection between client and server
-*
-* @return    0    connection was stopped
-* @return    -1   failed to stop connection
-*/
-int http_client_disconnect(void);
+int http_receive(char * headers);
