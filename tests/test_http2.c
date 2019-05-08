95ee03b1710d68104ecd47b2045e9e568875854f
sebastiancif <sebastian.cifuentes@ing.uchile.cl>
2019-05-08 00:17:48 -0400

    added read settings payload test. Loop bug found

--- tests/test_http2.c
+++ tests/test_http2.c
@@ -17,7 +17,7 @@ extern int read_frame(uint8_t *buff_read, frame_header_t *header);
    int read_bytes = 0;
    int incoming_bytes;
    while(read_bytes < n){
-     incoming_bytes = http_readfake(buff_read+read_bytes, n - read_bytes);
+     incoming_bytes = http_read(buff_read+read_bytes, n - read_bytes);
      /* incoming_bytes equals -1 means that there was an error*/
      if(incoming_bytes == -1){
        puts("Error in read function");
@@ -29,7 +29,7 @@ extern int read_frame(uint8_t *buff_read, frame_header_t *header);
  }
 
  // Toy write function
- int http_writefake(uint8_t *bytes, int length){
+ int http_write(uint8_t *bytes, int length){
    if(size+length > MAX_BUFFER_SIZE){
      return -1;
    }
@@ -40,7 +40,7 @@ extern int read_frame(uint8_t *buff_read, frame_header_t *header);
  }
 
 // Toy read function
-int http_readfake(uint8_t *bytes, int length){
+int http_read(uint8_t *bytes, int length){
   if(length > size){
       length = size;
   }
@@ -56,7 +56,6 @@ int http_readfake(uint8_t *bytes, int length){
 
 DEFINE_FFF_GLOBALS;
 FAKE_VALUE_FUNC(int, verify_setting, uint16_t, uint32_t);
-FAKE_VALUE_FUNC(int, http_write, uint8_t *, int);
 FAKE_VALUE_FUNC(int, create_settings_ack_frame, frame_t *, frame_header_t*);
 FAKE_VALUE_FUNC(int, frame_to_bytes, frame_t*, uint8_t*);
 FAKE_VALUE_FUNC(int, is_flag_set, uint8_t, uint8_t);
@@ -66,7 +65,6 @@ FAKE_VALUE_FUNC(int, create_settings_frame ,uint16_t*, uint32_t*, int, frame_t*,
 
 #define FFF_FAKES_LIST(FAKE)         \
     FAKE(verify_setting)             \
-    FAKE(http_write)                 \
     FAKE(create_settings_ack_frame)  \
     FAKE(frame_to_bytes)             \
     FAKE(is_flag_set)                \
@@ -94,6 +92,7 @@ void setUp(void){
 
   /* reset common FFF internal structures */
   FFF_RESET_HISTORY();
+  size = 0;
 }
 
 void test_init_variables(void){
@@ -166,11 +165,28 @@ void test_send_settings_ack(void){
   int rc = send_settings_ack();
   create_settings_ack_frame_fake.custom_fake = return_zero;
   frame_to_bytes_fake.custom_fake = return_24;
-  http_write_fake.custom_fake = return_24;
   TEST_ASSERT_MESSAGE(create_settings_ack_frame_fake.call_count = 1, "ACK creation not called");
   TEST_ASSERT_MESSAGE(frame_to_bytes_fake.call_count = 1, "frame to bytes not called");
-  TEST_ASSERT_MESSAGE(http_write_fake.call_count = 1, "http write not called");
   TEST_ASSERT_MESSAGE(rc == 0, "RC must be 0");
+}
+
+void test_read_settings_payload(void){
+  h2states_t dummy = {{1,1,1,1,1,1},
+                      {1,1,1,1,1,1},
+                      1};
+  frame_header_t header_ack = {0, 0x4, 0x0|0x1, 0x0, 0};
+  frame_header_t header_sett = {24, 0x4, 0x0, 0x0, 0};
+  int flag_returns[2] = {1, 0};
+  SET_RETURN_SEQ(is_flag_set, flag_returns, 2);
+  bytes_to_settings_payload_fake.custom_fake = return_24;
+  create_settings_ack_frame_fake.custom_fake = return_zero;
+  verify_setting_fake.custom_fake = return_zero;
+  frame_to_bytes_fake.custom_fake = return_24;
+  int rc = read_settings_payload(buffer, &header_ack, &dummy);
+  TEST_ASSERT_MESSAGE(rc == 0, "RC must be 0");
+  TEST_ASSERT_MESSAGE(is_flag_set_fake.call_count == 1, "is_flag_set must be called once");
+  TEST_ASSERT_MESSAGE(dummy.wait_setting_ack == 0, "ACK wait must be 0");
+  /*rc = read_settings_payload(buffer, &header_sett, &dummy);*/
 }
 
 void test_send_local_settings(void){
@@ -180,11 +196,9 @@ void test_send_local_settings(void){
                       0};
   create_settings_frame_fake.custom_fake = return_zero;
   frame_to_bytes_fake.custom_fake = return_24;
-  http_write_fake.custom_fake = return_24;
   int rc = send_local_settings(&dummy);
   TEST_ASSERT_MESSAGE(create_settings_frame_fake.call_count == 1, "create_settings_frame must be called once");
   TEST_ASSERT_MESSAGE(frame_to_bytes_fake.call_count == 1, "frame_to_bytes must be called once");
-  TEST_ASSERT_MESSAGE(http_write_fake.call_count == 1, "http_write must be called once");
   TEST_ASSERT_MESSAGE(dummy.wait_setting_ack == 1, "wait_setting_ack must be 1. Settings were sent");
   TEST_ASSERT_MESSAGE(rc == 0, "return code of send_local_settings must be 0");
 }
@@ -195,7 +209,6 @@ void test_client_init_connection(void){
   uint32_t init_vals[6] = {DEFAULT_HTS,DEFAULT_EP,DEFAULT_MCS,DEFAULT_IWS,DEFAULT_MFS,DEFAULT_MHLS};
   create_settings_frame_fake.custom_fake = return_zero;
   frame_to_bytes_fake.custom_fake = return_24;
-  http_write_fake.custom_fake = return_24;
   int rc = client_init_connection(&client);
   TEST_ASSERT_MESSAGE(client.local_settings[0] == init_vals[0], "HTS in local settings is not setted");
   TEST_ASSERT_MESSAGE(client.local_settings[1] == init_vals[1], "EP in local settings is not setted");
@@ -209,7 +222,6 @@ void test_client_init_connection(void){
   TEST_ASSERT_MESSAGE(client.remote_settings[3] == init_vals[3], "IWS in local settings is not setted");
   TEST_ASSERT_MESSAGE(client.remote_settings[4] == init_vals[4], "MFS in local settings is not setted");
   TEST_ASSERT_MESSAGE(client.remote_settings[5] == init_vals[5], "MHLS in local settings is not setted");
-  TEST_ASSERT_MESSAGE(http_write_fake.call_count == 2, "http_write must be called twice");
   TEST_ASSERT_MESSAGE(rc == 0, "return code of client_init_connection must be 0");
 }
 
@@ -219,7 +231,6 @@ void test_server_init_connection(void){
   uint32_t init_vals[6] = {DEFAULT_HTS,DEFAULT_EP,DEFAULT_MCS,DEFAULT_IWS,DEFAULT_MFS,DEFAULT_MHLS};
   create_settings_frame_fake.custom_fake = return_zero;
   frame_to_bytes_fake.custom_fake = return_24;
-  http_write_fake.custom_fake = return_24;
   char *preface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
   uint8_t preface_buff[24];
   uint8_t i = 0;
@@ -228,7 +239,7 @@ void test_server_init_connection(void){
     preface_buff[i] = preface[i];
     i++;
   }
-  int wrc = http_writefake(preface_buff,24);
+  int wrc = http_write(preface_buff,24);
   TEST_ASSERT_MESSAGE(wrc == 24, "Fake buffer not written");
   int rc = server_init_connection(&server);
   TEST_ASSERT_MESSAGE(server.local_settings[0] == init_vals[0], "HTS in local settings is not setted");
@@ -243,7 +254,7 @@ void test_server_init_connection(void){
   TEST_ASSERT_MESSAGE(server.remote_settings[3] == init_vals[3], "IWS in local settings is not setted");
   TEST_ASSERT_MESSAGE(server.remote_settings[4] == init_vals[4], "MFS in local settings is not setted");
   TEST_ASSERT_MESSAGE(server.remote_settings[5] == init_vals[5], "MHLS in local settings is not setted");
-  TEST_ASSERT_MESSAGE(rc==0, "return code of client_init_connection must be 0");
+  TEST_ASSERT_MESSAGE(rc==0, "return code of server_init_connection must be 0");
 }
 
 int main(void)
@@ -252,6 +263,7 @@ int main(void)
     UNIT_TEST(test_init_variables);
     UNIT_TEST(test_update_settings_table);
     UNIT_TEST(test_send_settings_ack);
+    UNIT_TEST(test_read_settings_payload);
     UNIT_TEST(test_send_local_settings);
     UNIT_TEST(test_client_init_connection);
     UNIT_TEST(test_server_init_connection);
