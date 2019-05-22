b50b88f4c5a60068902c4da86d6ab6c02c4e42e8
sebastiancif <seba@niclabs.cl>
2019-05-22 17:13:18 -0400

    little changes

--- src/http2utils.c
+++ src/http2utils.c
@@ -1,5 +1,4 @@
 #include "http2utils.h"
-#include "http_methods.h"
 
 uint8_t buffer[MAX_BUFFER_SIZE];
 int size = 0;
@@ -59,13 +58,13 @@ int verify_setting(uint16_t id, uint32_t value){
 *        -> n: number of bytes to read.
 * Output: The number of bytes written in buffer. -1 if error was found.
 */
-int read_n_bytes(uint8_t *buff_read, int n){
+int read_n_bytes(uint8_t *buff_read, int n,  hstates_t *hs){
   int read_bytes = 0;
   int incoming_bytes;
   while(read_bytes < n){
-    incoming_bytes = http_read(buff_read+read_bytes, n - read_bytes);
+    incoming_bytes = http_read(buff_read+read_bytes, n - read_bytes, hs);
     /* incoming_bytes equals -1 means that there was an error*/
-    if(incoming_bytes == -1){
+    if(incoming_bytes < 1){
       puts("Error in read function");
       return -1;
     }
