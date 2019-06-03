ffd2e40db5180848ad767a2c0e2b658ea209172b
Felipe Lalanne <felipe@lalanne.cl>
2019-06-03 17:29:32 -0400

    Remove check for errno in sock_create

--- src/sock.c
+++ src/sock.c
@@ -26,8 +26,6 @@ int sock_create(sock_t *sock)
 
     sock->fd = socket(AF_INET6, SOCK_STREAM, 0);
     if (sock->fd < 0) {
-        // Set errno if not given by socket() (should only be on a test mock setting)
-        if (errno == 0) errno = EAGAIN;
         sock->state = SOCK_CLOSED;
         return -1;
     }
