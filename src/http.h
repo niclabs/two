99b819fdeb522111d99fc6c728434cd9297980a6
Javiera Alegría <javi@niclabs.cl>
2019-06-04 16:55:11 -0400

    Add new information to http.h documentation

--- src/http.h
+++ src/http.h
@@ -13,21 +13,13 @@
 /*
  * Given a port number this function initialize a server
  *
+ * @param    hs      struct for server information
  * @param    port    port number
  *
  * @return   0       Server was successfully initialized
  * @return   -1      Server wasn't initialized
  */
 int http_init_server(hstates_t *hs, uint16_t port);
-
-
-/*
- * Stop and destroy server if it is running
- *
- * @return   0       Server was successfully destroyed
- * @return   -1      Server wasn't destroyed
- */
-int http_server_destroy(hstates_t *hs);
 
 
 /*
@@ -45,6 +37,7 @@ int http_set_function_to_path(char *callback, char *path);
 /*
  * Add a header and its value to the headers list
  *
+ * @param    hs        struct with headers information
  * @param    name      new headers name
  * @param    value     new headers value
  *
@@ -54,12 +47,24 @@ int http_set_function_to_path(char *callback, char *path);
 int http_set_header(hstates_t *hs, char *name, char *value);
 
 
+/*
+ * Stop and destroy server if it is running
+ *
+ * @param    hs      struct with server information
+ *
+ * @return   0       Server was successfully destroyed
+ * @return   -1      Server wasn't destroyed
+ */
+int http_server_destroy(hstates_t *hs);
+
+
 
 /*Client*/
 
 /*
  * Start a connection from client to server
  *
+ * @param    hs     struct for client information
  * @param    port   port number
  * @param    ip     adress
  *
@@ -68,23 +73,27 @@ int http_set_header(hstates_t *hs, char *name, char *value);
  */
 int http_client_connect(hstates_t *hs, uint16_t port, char *ip);
 
+
+/*
+ * Search by a value of a header in the header list
+ *
+ * @param    hs         struct with headers information
+ * @param    header     Header name
+ *
+ * @return              value finded
+ */
+char *http_get_header(hstates_t *hs, char *header);
+
+
 /*
  * Stop the connection between client and server
+ *
+ * @param    hs     struct with client information
  *
  * @return    0    connection was stopped
  * @return    -1   failed to stop connection
  */
 int http_client_disconnect(hstates_t *hs);
-
-/*
- * Search by a value of a header in the header list
- *
- * @param    header     Header name
- * @param    hs         struct with headers info
- *
- * @return              value finded
- */
-char *http_get_header(hstates_t *hs, char *header);
 
 
 #endif /* HTTP_H */
