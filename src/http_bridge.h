9dba8d8d9ab678779df98eebb3e57494ed41522a
Sebastián Cifuentes <seba@niclabs.cl>
2019-06-05 13:24:42 -0400

    added new flag to http struct

--- src/http_bridge.h
+++ src/http_bridge.h
@@ -58,6 +58,8 @@ typedef struct HTTP_STATES {
     h2states_t h2s;
     uint8_t table_count;
     table_pair_t header_list[HTTP2_MAX_HEADER_COUNT];
+    uint8_t new_headers; //boolean. Notifies HTTP if new headers were written
+    uint8_t keep_receiving; //boolean. Tells HTTP to keep receiving frames
 
 } hstates_t;
 
