d3d89d1a48688d270d3592ee9015bdcb2d2adc95
Javiera Alegría <javi@niclabs.cl>
2019-06-07 18:01:20 -0400

    First version of function get_receive in http_bridge.c

--- src/http_bridge.c
+++ src/http_bridge.c
@@ -12,6 +12,7 @@
 
 #include "http_bridge.h"
 #include "http.h"
+#include "http2.h"
 #include "logging.h"
 
 
@@ -71,15 +72,36 @@ int http_receive(hstates_t *hs)
 }
 
 
-int get_receive(char *path, char *headers)
+int get_receive(hstates_t *hs, char *path)
 {
-    (void)path;
-    (void)headers;
     // preparar respuesta
-    // buscar callback correspondiente a path
-    // llamar a callback y agregar datos a respuesta
-    // codificar respuesta usando http2
-    return -1;
+
+    callback_type_t callback;
+
+    if (hs->path_callback_list_count == 0) {
+        WARN("Path-callback list is empty");
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i <= hs->path_callback_list_count; i++) {
+        if (strncmp(hs->path_callback_list[i].name, path, strlen(path)) == 0) {
+            callback.cb = hs->path_callback_list[i].ptr;
+            break;
+        }
+        if (i == hs->path_callback_list_count) {
+            WARN("No function associated with this path");
+            return -1;
+        }
+    }
+
+    callback.cb(&hs->h_lists);
+
+    if(h2_send_headers(hs)<0){
+      return -1;
+    }
+
+    return 0;
 }
 
 
