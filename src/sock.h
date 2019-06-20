bcea5ec41698d67e79fe84e011cb7e8193cad87a
Felipe Lalanne <felipe@lalanne.cl>
2019-06-20 18:08:29 -0400

    Add contiki sock.h definitions

--- src/sock.h
+++ src/sock.h
@@ -9,20 +9,22 @@
 #include <stdint.h>
 
 #ifdef WITH_CONTIKI
-#include "net/ip/tcp-socket.h"
+#include "sys/process.h"
 
-#ifndef SOCK_INPUT_BUF_SIZE
-#define SOCK_INPUT_BUF_SIZE 512
+#ifndef SOCK_BUFFER_SIZE
+#define SOCK_BUFFER_SIZE 128
 #endif
 
-#ifndef SOCK_OUTPUT_BUF_SIZE
-#define SOCK_OUTPUT_BUF_SIZE 512
+#ifndef SOCK_MAX_SOCKETS
+#define SOCK_MAX_SOCKETS UIP_CONNS
 #endif
 
-typedef struct tcp_socket sock_socket_t;
+#define PROCESS_SOCK_WAIT(sock, thread) PROCESS_PT_SPAWN(&(sock)->pt, thread)
+
+typedef struct sock_socket * sock_socket_t;
 #else
 typedef int sock_socket_t;
-#endif
+#endif /* WITH_CONTIKI */
 
 
 /* Socket states */
@@ -39,6 +41,9 @@ typedef enum {
 typedef struct {
     sock_socket_t socket;
     int state;
+#ifdef WITH_CONTIKI
+    struct pt pt;
+#endif
 } sock_t;
 
 /**
@@ -65,6 +70,10 @@ int sock_listen(sock_t * server, uint16_t port);
 /**
  * Wait for connections on server socket. Server socket must be set to listen by calling
  * sock_listen() previously, otherwise an error will be returned.
+ *
+ * In contiki, the method does not block and returns 0 only if there is a connection already
+ * in order for the process to wait for a client, the macro PROCES_SOCk_WAIT_CLIENT must be used
+ * before. 
  * 
  * @param   server      pointer to server socket data structure (defined by the implementation)
  * @param   client      pointer to client socket data structure (it will be initialized by the function)
@@ -90,6 +99,10 @@ int sock_connect(sock_t * client, char * addr, uint16_t port);
 /**
  * Read len bytes from the socket into the specified buffer. The method will wait for timeout seconds
  * if timeout is greater than zero
+ *
+ * In contiki, the method does not block and returns 0 only if there is a connection already
+ * in order for the process to wait for new data, the macro PROCES_SOCk_WAIT_DATA must be used
+ * before. 
  * 
  * @param   sock    pointer to socket data structure to read from
  * @param   buf     buffer to read the data into
@@ -126,6 +139,29 @@ int sock_write(sock_t * sock, char * buf, int len);
  * @return  -1  on error (errno will be set appropriately)
  */
 int sock_destroy(sock_t * sock);
+
+#ifdef WITH_CONTIKI
+/**
+ * Wait for a new client connection to arrive
+ * given the provided server sock
+ *
+ * @param sock server sock configured and listening
+ */ 
+PT_THREAD(sock_wait_client(sock_t * sock));
+
+/**
+ * Wait for new data to arrive on the provided client sock
+ *
+ * @param sock connected client sock
+ **/
+PT_THREAD(sock_wait_data(sock_t * sock));
+
+/**
+ * Process sock macros
+ */
+#define PROCESS_SOCK_WAIT_CLIENT(sock) PROCESS_SOCK_WAIT(sock, sock_wait_client(sock))
+#define PROCESS_SOCK_WAIT_DATA(sock) PROCESS_SOCK_WAIT(sock, sock_wait_data(sock))
+#endif
 
 
 #endif /* SOCK_H */
