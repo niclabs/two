5062e1ba87b5a6df11d1a85e2060950eaf85b84a
Javiera Alegría <javi@niclabs.cl>
2019-06-21 16:41:49 -0400

    Fixed not existent function in client

--- src/client.c
+++ src/client.c
@@ -1,21 +1,18 @@
 #include "client.h"
 #include "logging.h"
 
-void pseudoclient(hstates_t *hs, uint16_t port, char *ip){
-  int rc = http_client_connect(hs,port, ip);
-  if(rc<0){
-    ERROR("in client connect");
-  } else {
+void pseudoclient(hstates_t *hs, uint16_t port, char *ip)
+{
+    int rc = http_client_connect(hs, port, ip);
 
-    rc = http_get(hs, "index", "example.org", "text");
-    if(rc<0){
-      ERROR("in http_get");
-    } else {
-
-      rc = http_start_client(hs);
-      if(rc<0){
-        ERROR("in http_start_client");
-      }
+    if (rc < 0) {
+        ERROR("in client connect");
     }
-  }
+    else {
+
+        rc = http_get(hs, "index", "example.org", "text");
+        if (rc < 0) {
+            ERROR("in http_get");
+        }
+    }
 }
