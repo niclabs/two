dd5f46784143d328e2ca31606741d66d27e5cf39
maite.gonzalez.mendoza@gmail.com <maite.gonzalez.mendoza@gmail.com>
2019-06-26 11:19:23 -0400

    rename cbuf read/write to pop/push in test_cbuf

--- tests/test_cbuf.c
+++ tests/test_cbuf.c
@@ -3,6 +3,8 @@
 #include "unit.h"
 #include "cbuf.h"
 
+
+
 void test_write_after_end_buffer(void)
 {
     char buf[8];
@@ -12,15 +14,15 @@ void test_write_after_end_buffer(void)
 
     int bytes = 0;
 
-    bytes = cbuf_write(&cbuf, "abcd", 4);
+    bytes = cbuf_push(&cbuf, "abcd", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcd", buf, 4);
 
-    bytes = cbuf_write(&cbuf, "efgh", 4);
+    bytes = cbuf_push(&cbuf, "efgh", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcdefgh", buf, 8);
 
-    bytes = cbuf_write(&cbuf, "ijk", 3);
+    bytes = cbuf_push(&cbuf, "ijk", 3);
     TEST_ASSERT_EQUAL_MESSAGE(0, bytes, "Should not keep writing after end of buffer");
     TEST_ASSERT_EQUAL_STRING_LEN("abcdefgh", buf, 8);
 }
@@ -34,34 +36,34 @@ void test_write_and_read(void)
 
     int bytes = 0;
 
-    bytes = cbuf_read(&cbuf, readbuf, 10);
+    bytes = cbuf_pop(&cbuf, readbuf, 10);
     TEST_ASSERT_EQUAL_MESSAGE(0, bytes, "Should read 0 bytes");
 
-    bytes = cbuf_write(&cbuf, "abcd", 4);
+    bytes = cbuf_push(&cbuf, "abcd", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcd", buf, 4);
 
-    bytes = cbuf_read(&cbuf, readbuf, 3);
+    bytes = cbuf_pop(&cbuf, readbuf, 3);
     TEST_ASSERT_EQUAL_MESSAGE(3, bytes, "Should read 3 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abc", readbuf, 3);
     
-    bytes = cbuf_write(&cbuf, "efgh", 4);
+    bytes = cbuf_push(&cbuf, "efgh", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcdefgh", buf, 8);
     
-    bytes = cbuf_read(&cbuf, readbuf, 4);
+    bytes = cbuf_pop(&cbuf, readbuf, 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should read 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("defg", readbuf, 4);
     
-    bytes = cbuf_write(&cbuf, "ijk", 3);
+    bytes = cbuf_push(&cbuf, "ijk", 3);
     TEST_ASSERT_EQUAL_MESSAGE(3, bytes, "Should write 3 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("ijkdefgh", buf, 8);
     
-    bytes = cbuf_read(&cbuf, readbuf, 4);
+    bytes = cbuf_pop(&cbuf, readbuf, 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should read 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("hijk", readbuf, 4);
 
-    bytes = cbuf_read(&cbuf, readbuf, 4);
+    bytes = cbuf_pop(&cbuf, readbuf, 4);
     TEST_ASSERT_EQUAL_MESSAGE(0, bytes, "Should read 0 bytes");
 }
 
@@ -76,7 +78,7 @@ void test_peek_buffer(void) {
     bytes = cbuf_peek(&cbuf, readbuf, 10);
     TEST_ASSERT_EQUAL_MESSAGE(0, bytes, "Should read 0 bytes");
 
-    bytes = cbuf_write(&cbuf, "abcd", 4);
+    bytes = cbuf_push(&cbuf, "abcd", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcd", buf, 4);
 
@@ -85,11 +87,11 @@ void test_peek_buffer(void) {
     TEST_ASSERT_EQUAL_STRING_LEN("abc", readbuf, 3);
     TEST_ASSERT_EQUAL_MESSAGE(4, cbuf_len(&cbuf), "Buffer length must not change with peek");
     
-    bytes = cbuf_write(&cbuf, "efgh", 4);
+    bytes = cbuf_push(&cbuf, "efgh", 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should write 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcdefgh", buf, 8);
     
-    bytes = cbuf_read(&cbuf, readbuf, 4);
+    bytes = cbuf_pop(&cbuf, readbuf, 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, bytes, "Should read 4 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("abcd", readbuf, 4);
     
@@ -98,7 +100,7 @@ void test_peek_buffer(void) {
     TEST_ASSERT_EQUAL_STRING_LEN("efgh", readbuf, 4);
     TEST_ASSERT_EQUAL_MESSAGE(4, cbuf_len(&cbuf), "Buffer length must not change with peek");
     
-    bytes = cbuf_write(&cbuf, "ijk", 3);
+    bytes = cbuf_push(&cbuf, "ijk", 3);
     TEST_ASSERT_EQUAL_MESSAGE(3, bytes, "Should write 3 bytes");
     TEST_ASSERT_EQUAL_STRING_LEN("ijkdefgh", buf, 8);
     
