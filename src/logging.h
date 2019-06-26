abe4afdedb6c8991109e12059d1d81fbd7fcafb5
Felipe Lalanne <felipe@lalanne.cl>
2019-06-25 21:27:01 -0400

    Rename logging print methods

--- src/logging.h
+++ src/logging.h
@@ -30,18 +30,18 @@ typedef enum
 
 #ifdef WITH_CONTIKI
 #define LOG_EXIT(code) process_exit(PROCESS_CURRENT())
-#define LOG_PRINTF(...) printf(__VA_ARGS__)
+#define LOG_PRINT(...) printf(__VA_ARGS__)
 #else
 #define LOG_EXIT(code) exit(code)
-#define LOG_PRINTF(...) fprintf(stderr, __VA_ARGS__)
+#define LOG_PRINT(...) fprintf(stderr, __VA_ARGS__)
 #endif
 
 #ifndef ENABLE_DEBUG
 #define ENABLE_DEBUG (0)
-#define LOG_CONTEXT(func, file, line)
+#define LOG_PRINT_CONTEXT(func, file, line)
 #else
 #define LEVEL (DEBUG)
-#define LOG_CONTEXT(func, file, line) LOG_PRINTF("; at %s:%d in %s()", file, line, func)
+#define LOG_PRINT_CONTEXT(func, file, line) LOG_PRINT("; at %s:%d in %s()", file, line, func)
 #endif
 
 #ifndef LEVEL
@@ -55,23 +55,23 @@ typedef enum
     {                                                                                                                \
         if (level >= LEVEL)                                                                                          \
         {                                                                                                            \
-            LOG_PRINTF("[" LEVEL_STR(level) "] " msg, ##__VA_ARGS__);                                                \
+            LOG_PRINT("[" LEVEL_STR(level) "] " msg, ##__VA_ARGS__);                                                 \
             switch (level)                                                                                           \
             {                                                                                                        \
             case DEBUG:                                                                                              \
             case INFO:                                                                                               \
             case WARN:                                                                                               \
-                LOG_CONTEXT(func, file, line);                                                                       \
-                LOG_PRINTF("\n");                                                                                    \
+                LOG_PRINT_CONTEXT(func, file, line);                                                                 \
+                LOG_PRINT("\n");                                                                                     \
                 break;                                                                                               \
             case ERROR:                                                                                              \
             case FATAL:                                                                                              \
                 if (errno > 0)                                                                                       \
                 {                                                                                                    \
-                    LOG_PRINTF(" (%s)", strerror(errno));                                                            \
+                    LOG_PRINT(" (%s)", strerror(errno));                                                             \
                 }                                                                                                    \
-                LOG_CONTEXT(func, file, line);                                                                       \
-                LOG_PRINTF("\n");                                                                                    \
+                LOG_PRINT_CONTEXT(func, file, line);                                                                 \
+                LOG_PRINT("\n");                                                                                     \
                 break;                                                                                               \
             }                                                                                                        \
         }                                                                                                            \
