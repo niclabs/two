24515ee26fddec88192f8bb5867bfd192d5250a3
Felipe Lalanne <flalanne@niclabs.cl>
2019-06-27 11:44:59 -0400

    Improved logging.h to reduce code size

--- src/logging.h
+++ src/logging.h
@@ -2,11 +2,11 @@
  * Logging and debugging API
  *
  * Defines the DEBUG() INFO() WARN() and ERROR() macros that will print
- * a message depending on the level defined by the variable LEVEL (default is INFO)
+ * a message depending on the level defined by the variable LOG_LEVEL (default is INFO)
  *
  * When debugging, it might be useful to set level to DEBUG, either by setting the variable
- * ENABLE_DEBUG to 1, by using the -DENABLE_DEBUG=1 on compilation, or by directly setting the LEVEL
- * variable to DEBUG, by using -DLEVEL=DEBUG
+ * ENABLE_DEBUG to 1, by using the -DENABLE_DEBUG=1 on compilation, or by directly setting the LOG_LEVEL
+ * variable to DEBUG, by using -DLOG_LEVEL=LOG_LEVEL_DEBUG
  *
  * @author Felipe Lalanne <flalanne@niclabs.cl>
  */
@@ -18,15 +18,13 @@
 #include <stdio.h>
 #include <errno.h>
 
-typedef enum
-{
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
-    FATAL,
-    OFF
-} level_t;
+// Log levels
+#define LOG_LEVEL_DEBUG (1)
+#define LOG_LEVEL_INFO  (2)
+#define LOG_LEVEL_WARN  (3)
+#define LOG_LEVEL_ERROR (4)
+#define LOG_LEVEL_FATAL (5)
+#define LOG_LEVEL_OFF   (6)
 
 #ifdef WITH_CONTIKI
 #define LOG_EXIT(code) process_exit(PROCESS_CURRENT())
@@ -36,62 +34,63 @@ typedef enum
 #define LOG_PRINT(...) fprintf(stderr, __VA_ARGS__)
 #endif
 
-#ifndef ENABLE_DEBUG
-#define ENABLE_DEBUG (0)
-#define LOG_PRINT_CONTEXT(func, file, line)
-#else
-#define LEVEL (DEBUG)
-#define LOG_PRINT_CONTEXT(func, file, line) LOG_PRINT("; at %s:%d in %s()", file, line, func)
+#ifdef ENABLE_DEBUG
+#define LOG_LEVEL (LOG_LEVEL_DEBUG)
 #endif
 
-#ifndef LEVEL
-#define LEVEL (INFO)
+#ifndef LOG_LEVEL
+#define LOG_LEVEL (LOG_LEVEL_INFO)
 #endif
 
-#define LEVEL_STR(level) #level
-
-#define LOG(level, func, file, line, msg, ...)                                                                       \
-    do                                                                                                               \
-    {                                                                                                                \
-        if (level >= LEVEL)                                                                                          \
-        {                                                                                                            \
-            LOG_PRINT("[" LEVEL_STR(level) "] " msg, ##__VA_ARGS__);                                                 \
-            switch (level)                                                                                           \
-            {                                                                                                        \
-            case DEBUG:                                                                                              \
-            case INFO:                                                                                               \
-            case WARN:                                                                                               \
-                LOG_PRINT_CONTEXT(func, file, line);                                                                 \
-                LOG_PRINT("\n");                                                                                     \
-                break;                                                                                               \
-            case ERROR:                                                                                              \
-            case FATAL:                                                                                              \
-                if (errno > 0)                                                                                       \
-                {                                                                                                    \
-                    LOG_PRINT(" (%s)", strerror(errno));                                                             \
-                }                                                                                                    \
-                LOG_PRINT_CONTEXT(func, file, line);                                                                 \
-                LOG_PRINT("\n");                                                                                     \
-                break;                                                                                               \
-            }                                                                                                        \
-        }                                                                                                            \
-        if (level == FATAL) LOG_EXIT(EXIT_FAILURE);                                                                  \
-    } while (0)
+#if LOG_LEVEL_DEBUG >= LOG_LEVEL
+#define LOG(level, func, file, line, msg, ...) LOG_PRINT("[" #level "] " msg "; at %s:%d in %s()\n", ## __VA_ARGS__, file, line, func)
+#else 
+#define LOG(level, func, file, line, msg, ...) LOG_PRINT("[" #level "] " msg "\n", ## __VA_ARGS__)
+#endif
 
 // Macro to print debugging messages
+#if LOG_LEVEL_DEBUG >= LOG_LEVEL
 #define DEBUG(...) LOG(DEBUG, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
 
 // Macro to print information messages
+#if LOG_LEVEL_INFO >= LOG_LEVEL
 #define INFO(...) LOG(INFO, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#define INFO(...)
+#endif
 
 // Macro to print warning messages
+#if LOG_LEVEL_WARN >= LOG_LEVEL
 #define WARN(...) LOG(WARN, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#define WARN(...)
+#endif
 
 // Macro to print error messages
-#define ERROR(...) LOG(ERROR, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL_ERROR >= LOG_LEVEL
+#define ERROR(msg, ...)                                                                                             \
+    do {                                                                                                            \
+        if (errno > 0) { LOG(ERROR, __func__, __FILE__, __LINE__, msg " (%s)", ## __VA_ARGS__, strerror(errno)); }  \
+        else { LOG(ERROR, __func__, __FILE__, __LINE__, msg, ## __VA_ARGS__); }                                     \
+    } while (0)
+#else
+#define ERROR(...)
+#endif
 
 // Macro to print fatal error messages
-#define FATAL(...) LOG(FATAL, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#if LOG_LEVEL_FATAL >= LOG_LEVEL
+#define FATAL(msg, ...)                                                                                             \
+    do {                                                                                                            \
+        if (errno > 0) { LOG(FATAL, __func__, __FILE__, __LINE__, msg " (%s)", ## __VA_ARGS__, strerror(errno)); }  \
+        else { LOG(FATAL, __func__, __FILE__, __LINE__, msg, ## __VA_ARGS__); }                                     \
+        LOG_EXIT(EXIT_FAILURE);                                                                                     \
+    } while (0)
+#else
+#define FATAL(...)
+#endif
 
 // User messages
 #ifndef DISABLE_PRINTF
