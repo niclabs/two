197ce544b2a52d649c32602d736471ba2fd2c5b1
Javiera Alegría <javi@niclabs.cl>
2019-07-05 16:03:27 -0400

    Initialized data buffer memory in client.c

--- src/client.c
+++ src/client.c
@@ -10,6 +10,8 @@ void pseudoclient(hstates_t *hs, uint16_t port, char *ip)
     }
     else {
         response_received_type_t rr;
+        uint8_t misdatos[128];
+        rr.data = misdatos;
         rc = http_get(hs, "index", "example.org", "text", &rr);
         if (rc < 0) {
             ERROR("in http_get");
