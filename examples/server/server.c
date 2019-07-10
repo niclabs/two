6e93f669a5670e380d5022e02e8c63c03b296b47
Felipe Lalanne <flalanne@niclabs.cl>
2019-07-10 12:43:08 -0400

    server.c now builds to bin/server

---
+++ examples/server/server.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include "http.h"
+#include "logging.h"
+
+hstates_t http_server_state;
+
+void cleanup(int signal)
+{
+    (void)signal;
+
+    INFO("Ctrl-C received. Terminating");
+    http_client_disconnect(&http_server_state);
+    http_server_destroy(&http_server_state);
+}
+
+int send_text(headers_data_lists_t *headers_and_data)
+{
+    http_set_header(headers_and_data, "etag", "Hello world!");
+    http_set_header(headers_and_data, "expires", "Thu, 23 Jul");
+    http_set_data(headers_and_data, (uint8_t *)"Hello world!",12 );
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        PRINTF("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    uint16_t port = atoi(argv[1]);
+    if (port < 0) {
+        PRINTF("Invalid port given");
+        return 1;
+    }
+
+    int rc = http_init_server(&http_server_state, port);
+
+    if (rc < 0) {
+        ERROR("in init server");
+    }
+    else {
+        callback_type_t callback;
+        callback.cb = send_text;
+        rc = http_set_function_to_path(&http_server_state, callback, "index");
+        if (rc < 0) {
+            ERROR("in http_set_function_to_path");
+        }
+        else {
+
+            rc = http_start_server(&http_server_state);
+            if (rc < 0) {
+                ERROR("in start server");
+            }
+        }
+    }
+}
--- src/server.h
+++
@@ -1,3 +0,0 @@
-#include "http.h"
-
-void pseudoserver(hstates_t *hs, uint16_t port);
