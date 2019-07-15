19570decbd686af2c7ad3014c5171da56c9cfcdf
Felipe Lalanne <felipe@lalanne.cl>
2019-07-15 11:13:58 -0400

    Update http_headers implementation to pass all tests

--- src/http_headers.c
+++ src/http_headers.c
@@ -5,10 +5,10 @@
 #include "http_headers.h"
 
 
-
 int http_headers_init(http_headers_t *headers)
 {
-    memset(headers, 0, sizeof(http_headers_t));
+    memset(headers, 0, sizeof(*headers));
+    return 0;
 }
 
 int http_headers_set_header(http_headers_t *headers, const char *name, const char *value)
@@ -18,13 +18,8 @@ int http_headers_set_header(http_headers_t *headers, const char *name, const cha
         return -1;
     }
 
-    if (strlen(name) >= HTTP_MAX_HEADER_NAME_SIZE || strlen(value) >= HTTP_MAX_HEADER_VALUE_SIZE) {
+    if (strlen(name) > HTTP_MAX_HEADER_NAME_SIZE || strlen(value) > HTTP_MAX_HEADER_VALUE_SIZE) {
         errno = EINVAL;
-        return -1;
-    }
-
-    if (headers->size >= HTTP_MAX_HEADER_COUNT) {
-        errno = ENOMEM;
         return -1;
     }
 
@@ -32,32 +27,53 @@ int http_headers_set_header(http_headers_t *headers, const char *name, const cha
         // case insensitive name comparison of headers
         if (strncasecmp(headers->list[i].name, name, HTTP_MAX_HEADER_NAME_SIZE) == 0) {
             // found a header with the same name
-            int len = strlen(value);
-            memcpy(headers->list[i].value, value, len);
-            if (len < HTTP_MAX_HEADER_VALUE_SIZE) {
-                // set the rest of the memory to 0
-                memset(headers->list[i].value + len, 0, HTTP_MAX_HEADER_VALUE_SIZE - len);
-            }
+            strncpy(headers->list[i].value, value, HTTP_MAX_HEADER_VALUE_SIZE);
             return 0;
         }
     }
 
-    int len;
-
+    if (headers->size >= HTTP_MAX_HEADER_COUNT) {
+        errno = ENOMEM;
+        return -1;
+    }
 
     // Set header name
-    len = strlen(name);
-    memcpy(headers->list[headers->size].name, name, len);
-    if (len < HTTP_MAX_HEADER_NAME_SIZE) {
-        memset(headers->list[headers->size].name + len, 0, HTTP_MAX_HEADER_NAME_SIZE - len);
-    }
+    strncpy(headers->list[headers->size].name, name, HTTP_MAX_HEADER_NAME_SIZE);
 
     // Set header value
-    len = strlen(value);
-    memcpy(headers->list[headers->size].value, value, len);
-    if (len < HTTP_MAX_HEADER_VALUE_SIZE) {
-        memset(headers->list[headers->size].value + len, 0, HTTP_MAX_HEADER_VALUE_SIZE - len);
-    }
+    strncpy(headers->list[headers->size].value, value, HTTP_MAX_HEADER_VALUE_SIZE);
+
+    // Update header count
+    headers->size++;
 
     return 0;
+}
+
+int http_headers_get_header(http_headers_t *headers, const char *name, char *value)
+{
+    if (headers == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (strlen(name) > HTTP_MAX_HEADER_NAME_SIZE) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    for (int i = 0; i < headers->size; i++) {
+        // case insensitive name comparison of headers
+        if (strncasecmp(headers->list[i].name, name, HTTP_MAX_HEADER_NAME_SIZE) == 0) {
+            // found a header with the same name
+            strncpy(value, headers->list[i].value, HTTP_MAX_HEADER_VALUE_SIZE);
+            return 0;
+        }
+    }
+    return -1;
+}
+
+
+int http_headers_size(http_headers_t *headers)
+{
+    return headers->size;
 }
