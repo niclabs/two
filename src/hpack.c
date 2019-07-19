c501e8bb54d4ed2a50f34dcdc7a923a003325327
Pablo Aliaga <pablooaliaga@gmail.com>
2019-07-19 17:24:04 -0400

    Add documentation for decode_header_block

--- src/hpack.c
+++ src/hpack.c
@@ -610,6 +610,10 @@ int decode_header(uint8_t *bytes, hpack_preamble_t preamble, char *name, char *v
     }
 }
 
+//decodes an array of headers,
+//as it decodes one, the pointer of the headers move forwards
+//also has to update the decoded header lists
+//returns the amount of octets in which the pointer has move to read all the headers
 int decode_header_block(uint8_t *header_block, uint8_t header_block_size, headers_data_lists_t *h_list)
 {
     int pointer = 0;
