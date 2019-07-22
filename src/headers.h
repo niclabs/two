6f595a21ddea7058e65937a45da50ec11bdd1511
Felipe Lalanne <flalanne@niclabs.cl>
2019-07-22 11:45:39 -0400

    Update headers API

--- src/headers.h
+++ src/headers.h
@@ -1,12 +1,6 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
-#ifndef CONF_MAX_HEADER_COUNT
-#define MAX_HEADER_COUNT (16)
-#else
-#define MAX_HEADER_COUNT (CONF_MAX_HEADER_COUNT)
-#endif
-
 #ifndef CONF_MAX_HEADER_NAME_SIZE
 #define MAX_HEADER_NAME_SIZE (32)
 #else
@@ -25,6 +19,16 @@ typedef struct {
     char value[MAX_HEADER_VALUE_SIZE];
 } header_t;
 
+typedef struct {
+    header_t * headers;
+    int count;
+    int maxlen;
+} headers_t;
 
+int headers_init(headers_t * headers, header_t * hlist, int maxlen);
+int headers_add(headers_t * headers, const char * name, const char * value);
+int headers_set(headers_t * headers, const char * name, const char * value);
+int headers_get(header_t * headers, const char * name, char * value);
+int headers_count(header_t * headers);
 
 #endif
