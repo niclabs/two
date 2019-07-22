b027d669680009cbce27161d917a204245fea940
Gabriel Norambuena <gabo.norambuena@gmail.com>
2019-07-22 17:18:15 -0400

    Modified test for hpack_huffman according to new api

--- tests/test_hpack_huffman.c
+++ tests/test_hpack_huffman.c
@@ -21,54 +21,54 @@ void setUp(void) {
 void test_hpack_huffman_encode_random(void){
     int8_t rs = -1;
     huffman_encoded_word_t result;
-    rs = hpack_huffman_encode(&huffman_tree, &result, 0);
+    rs = hpack_huffman_encode(&result, 0);
     /*Test first value*/
     TEST_ASSERT_EQUAL(0x1ff8, result.code);
     TEST_ASSERT_EQUAL(13, result.length);
 
     /*Test 10 random values*/
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 37);
+    rs = hpack_huffman_encode(&result, 37);
     TEST_ASSERT_EQUAL(0x15, result.code);
     TEST_ASSERT_EQUAL(6, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 46);
+    rs = hpack_huffman_encode(&result, 46);
     TEST_ASSERT_EQUAL(0x17, result.code);
     TEST_ASSERT_EQUAL(6, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 75);
+    rs = hpack_huffman_encode(&result, 75);
     TEST_ASSERT_EQUAL(0x66, result.code);
     TEST_ASSERT_EQUAL(7, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 88);
+    rs = hpack_huffman_encode(&result, 88);
     TEST_ASSERT_EQUAL(0xfc, result.code);
     TEST_ASSERT_EQUAL(8, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 181);
+    rs = hpack_huffman_encode(&result, 181);
     TEST_ASSERT_EQUAL(0x3fffe1, result.code);
     TEST_ASSERT_EQUAL(22, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 182);
+    rs = hpack_huffman_encode(&result, 182);
     TEST_ASSERT_EQUAL(0x7fffee, result.code);
     TEST_ASSERT_EQUAL(23, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 210);
+    rs = hpack_huffman_encode(&result, 210);
     TEST_ASSERT_EQUAL(0x3ffffe6, result.code);
     TEST_ASSERT_EQUAL(26, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 213);
+    rs = hpack_huffman_encode(&result, 213);
     TEST_ASSERT_EQUAL(0x3ffffe7, result.code);
     TEST_ASSERT_EQUAL(26, result.length);
     TEST_ASSERT_EQUAL(0, rs);
 
-    rs = hpack_huffman_encode(&huffman_tree, &result, 251);
+    rs = hpack_huffman_encode(&result, 251);
     TEST_ASSERT_EQUAL(0x7ffffed, result.code);
     TEST_ASSERT_EQUAL(27, result.length);
     TEST_ASSERT_EQUAL(0, rs);
@@ -89,7 +89,7 @@ void test_hpack_huffman_encode_unique(void) {
     huffman_encoded_word_t result;
 
     for(int i = 0; i < HUFFMAN_TABLE_SIZE; i++){
-        rs = hpack_huffman_encode(&huffman_tree, &result, i);
+        rs = hpack_huffman_encode(&result, i);
         TEST_ASSERT_EQUAL(0, rs);
         codes[i] = result.code;
         for(int j = i - 1; j >= 0; j--) {
@@ -106,8 +106,8 @@ void test_hpack_huffman_encode_twice(void) {
     huffman_encoded_word_t result2;
 
     for(int i = 0; i < HUFFMAN_TABLE_SIZE; i++){
-        rs = hpack_huffman_encode(&huffman_tree, &result, i);
-        rs = hpack_huffman_encode(&huffman_tree, &result2, i);
+        rs = hpack_huffman_encode(&result, i);
+        rs = hpack_huffman_encode(&result2, i);
 
         TEST_ASSERT_EQUAL(result.code, result2.code);
         TEST_ASSERT_EQUAL(result.length, result2.length);
