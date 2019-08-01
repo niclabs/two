d1201a836907a05f698030afca78cfd9279afed0
Gabriel Norambuena <gabo.norambuena@gmail.com>
2019-08-01 10:54:26 -0400

    Delete unused byte_to_8bits_string function in hpack_utils

--- src/hpack_utils.c
+++ src/hpack_utils.c
@@ -1,30 +1,6 @@
 #include "hpack_utils.h"
 #include <stdint.h>
-//
-// Created by maite on 30-05-19.
-//
 
-/*
- * pass a byte b to string bits in str
- * "str" must be a char[9] for allocate string with 8chars + '/0'
- * Returns 0 if ok.
- */
-int byte_to_8bits_string(uint8_t b, char *str)
-{
-    uint8_t aux = (uint8_t)128;
-
-    for (uint8_t i = 0; i < 8; i++) {
-        if (b & aux) {
-            str[i] = '1';
-        }
-        else {
-            str[i] = '0';
-        }
-        aux = aux >> 1;
-    }
-    str[8] = '\0';
-    return 0;
-}
 /*
  * Function: hpack_utils_read_bits_from_bytes
  * Reads bits from a buffer of bytes (max number of bits it can read is 32).
--- src/hpack_utils.h
+++ src/hpack_utils.h
@@ -12,7 +12,6 @@ typedef enum {
 } hpack_preamble_t;
 
 
-int byte_to_8bits_string(uint8_t b, char* str);
 uint32_t hpack_utils_read_bits_from_bytes(uint16_t current_bit_pointer, uint8_t number_of_bits_to_read, uint8_t *buffer);
 int8_t hpack_utils_check_can_read_buffer(uint16_t current_bit_pointer, uint8_t number_of_bits_to_read, uint8_t buffer_size);
 int hpack_utils_log128(uint32_t x);
