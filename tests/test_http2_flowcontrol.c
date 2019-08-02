4a896d57c30d9219241eb70a54fe483c99dd90a6
Valeria Guidotti <vale@niclabs.cl>
2019-08-02 12:46:04 -0400

    add tests for flow_control_receive_data

--- tests/test_http2_flowcontrol.c
+++ tests/test_http2_flowcontrol.c
@@ -49,11 +49,34 @@ void test_decrease_window_used(void){
   TEST_ASSERT_MESSAGE(rc == 0, "Return code must be 0");
   TEST_ASSERT_MESSAGE(window.window_used == 20, "Size of window used must be 20");
 }
+
+void test_flow_control_receive_data(void){
+  hstates_t st;
+  st.h2s.incoming_window.window_size = 2888;
+  st.h2s.incoming_window.window_used = 0;
+  int rc = flow_control_receive_data(&st, 10);
+  TEST_ASSERT_MESSAGE(rc == 0, "Return code must be 0");
+  TEST_ASSERT_MESSAGE(st.h2s.incoming_window.window_used == 10, "Size of window used must be 10");
+  rc = flow_control_receive_data(&st, 0);
+  TEST_ASSERT_MESSAGE(rc == 0, "Return code must be 0");
+  TEST_ASSERT_MESSAGE(st.h2s.incoming_window.window_used == 10, "Size of window used must be 10");
+}
+
+void test_flow_control_receive_data_error(void){
+  hstates_t st;
+  st.h2s.incoming_window.window_size = 2888;
+  st.h2s.incoming_window.window_used = 0;
+  int rc = flow_control_receive_data(&st, 2900);
+  TEST_ASSERT_MESSAGE(rc == -1, "Return code must be -1 (length > window_available)");
+  TEST_ASSERT_MESSAGE(st.h2s.incoming_window.window_used == 0, "Size of window used must be 0");
+}
 int main(void)
 {
   UNIT_TESTS_BEGIN();
   UNIT_TEST(test_get_window_available_size);
   UNIT_TEST(test_increase_window_used);
   UNIT_TEST(test_decrease_window_used);
+  UNIT_TEST(test_flow_control_receive_data);
+  UNIT_TEST(test_flow_control_receive_data_error);
   return UNIT_TESTS_END();
 }
