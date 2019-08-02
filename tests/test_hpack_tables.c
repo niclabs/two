892df5482daa4b7b787f3d893124af39fc6cb41f
Pablo Aliaga <pablooaliaga@gmail.com>
2019-08-02 13:20:04 -0400

    Added dynamic table add_find_entry test
    WORKS!!

--- tests/test_hpack_tables.c
+++ tests/test_hpack_tables.c
@@ -64,11 +64,82 @@ void test_hpack_tables_static_find_entry_name(void)
     TEST_ASSERT_EQUAL_STRING(name, expected_name[i]);
   }
 }
+
+void test_hpack_tables_dynamic_add_find_entry(void)
+{
+  uint32_t dynamic_table_max_size = 500;
+  header_pair_t table[hpack_tables_get_table_length(dynamic_table_max_size)];
+  hpack_dynamic_table_t dynamic_table;
+
+  hpack_tables_init_dynamic_table(&dynamic_table, dynamic_table_max_size, table);
+
+  char *new_names[] = {"hola","sol3","bien1"};
+  char *new_values[] = {"chao","luna4","mal2"};
+
+  char name[30];
+  char value[20];
+
+  TEST_ASSERT_EQUAL(0,dynamic_table.first);
+  TEST_ASSERT_EQUAL(0,dynamic_table.next);
+
+  //ITERATION 1, add first entry
+  hpack_tables_dynamic_table_add_entry(&dynamic_table, new_names[0], new_values[0]);
+  TEST_ASSERT_EQUAL(0, dynamic_table.first);
+  TEST_ASSERT_EQUAL(1, dynamic_table.next);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 62, name, value); //62 -> first value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[0], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[0], value);
+
+  //ITERATION 2, add second entry
+  hpack_tables_dynamic_table_add_entry(&dynamic_table, new_names[1], new_values[1]);
+  TEST_ASSERT_EQUAL(0, dynamic_table.first);
+  TEST_ASSERT_EQUAL(2, dynamic_table.next);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 62, name, value); //62 -> first value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[1], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[1], value);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 63, name, value); //63 -> second value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[0], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[0], value);
+
+  //ITERATION 3, add third entry
+  hpack_tables_dynamic_table_add_entry(&dynamic_table, new_names[2], new_values[2]);
+  TEST_ASSERT_EQUAL(0, dynamic_table.first);
+  TEST_ASSERT_EQUAL(3, dynamic_table.next);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 62, name, value); //62 -> first value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[2], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[2], value);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 63, name, value); //63 -> second value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[1], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[1], value);
+
+  memset(name, 0, sizeof(name));
+  memset(value, 0, sizeof(value));
+  hpack_tables_dynamic_find_entry_name_and_value(&dynamic_table, 64, name, value); //64 -> third value of dynamic table
+  TEST_ASSERT_EQUAL_STRING(new_names[0], name);
+  TEST_ASSERT_EQUAL_STRING(new_values[0], value);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
     UNIT_TEST(test_hpack_tables_find_index);
     UNIT_TEST(test_hpack_tables_static_find_entry_name_and_value);
     UNIT_TEST(test_hpack_tables_static_find_entry_name);
+    UNIT_TEST(test_hpack_tables_dynamic_add_find_entry);
     return UNITY_END();
 }
