dd45824cc1073122c710c15f2bff9eb1a29e31ad
Sebastián Cifuentes <seba@niclabs.cl>
2019-08-14 16:02:49 -0400

    removed tests from http2utils testing

--- tests/test_http2utils.c
+++ tests/test_http2utils.c
@@ -18,51 +18,6 @@ void setUp(void) {
     FFF_RESET_HISTORY();
 }
 
-void test_verify_setting(void){
-  int rc;
-  rc = verify_setting(HEADER_TABLE_SIZE, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for HTS");
-  rc = verify_setting(ENABLE_PUSH, 0);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for EP");
-  rc = verify_setting(ENABLE_PUSH, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for EP");
-  rc = verify_setting(MAX_CONCURRENT_STREAMS, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for MCS");
-  rc = verify_setting(INITIAL_WINDOW_SIZE, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for IWS");
-  rc = verify_setting(MAX_CONCURRENT_STREAMS, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for MCS");
-  rc = verify_setting(MAX_FRAME_SIZE, 16384);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for MFS");
-  rc = verify_setting(MAX_FRAME_SIZE, 16777214);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for MFS");
-  rc = verify_setting(MAX_HEADER_LIST_SIZE, 1);
-  TEST_ASSERT_MESSAGE(rc == 0, "rc must be 0, valid value was given for MHLS");
-  rc = verify_setting(0x20, 1);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1, invalid parameter");
-}
-
-void test_verify_setting_errors(void){
-  int rc;
-  rc = verify_setting(0x7, 3);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (invalid id)");
-  rc = verify_setting(HEADER_TABLE_SIZE, 0);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (invalid header table size)");
-  rc = verify_setting(ENABLE_PUSH, 2);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (invalid enable push)");
-  rc = verify_setting(MAX_CONCURRENT_STREAMS, 0);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (invalid max concurretn streams)");
-  rc = verify_setting(INITIAL_WINDOW_SIZE, 2147483648);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (value greater than max for initial window size)");
-  rc = verify_setting(MAX_FRAME_SIZE, 16382);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (value lower than min for max frame size)");
-  rc = verify_setting(MAX_FRAME_SIZE, 16777216);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (value greater than max for max frame size)");
-  rc = verify_setting(MAX_HEADER_LIST_SIZE, 0);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (value equals zero for max_header_list_size)");
-  rc = verify_setting(0x9, 0);
-  TEST_ASSERT_MESSAGE(rc == -1, "rc must be -1 (invalid id)");
-}
 void test_read_n_bytes(void){
   uint8_t buff[HTTP2_MAX_BUFFER_SIZE];
   hstates_t st;
@@ -136,8 +91,6 @@ void test_read_setting_from_errors(void){
 int main(void)
 {
     UNIT_TESTS_BEGIN();
-    UNIT_TEST(test_verify_setting);
-    UNIT_TEST(test_verify_setting_errors);
     UNIT_TEST(test_read_n_bytes);
     UNIT_TEST(test_read_n_bytes_error);
     UNIT_TEST(test_prepare_new_stream);
