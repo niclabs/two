3920828904ccf52b71930dca5dd70bef1d807ca5
Sebastián Cifuentes <seba@niclabs.cl>
2019-08-14 15:54:19 -0400

    moved send_connection_error function on http2.c

--- src/http2.c
+++ src/http2.c
@@ -81,6 +81,70 @@ int read_frame(uint8_t *buff_read, frame_header_t *header, hstates_t *st){
     return 0;
 }
 
+
+/*
+* Function: send_goaway
+* Given an hstates_t struct and a valid error code, sends a GOAWAY FRAME to endpoint.
+* If additional debug data (opaque data) is included it must be written on the
+* given hstates_t.h2s.debug_data_buffer buffer with its corresponding size on the
+* hstates_t.h2s.debug_size variable.
+* IMPORTANT: RFC 7540 section 6.8 indicates that a gracefully shutdown should have
+* an 2^31-1 value on the last_stream_id field and wait at least one RTT before sending
+* a goaway frame with the last stream id. This implementation doesn't.
+* Input: ->st: pointer to hstates_t struct where connection variables are stored
+*        ->error_code: error code for GOAWAY FRAME (RFC 7540 section 7)
+* Output: 0 if no errors were found, -1 if not
+*/
+int send_goaway(hstates_t *st, uint32_t error_code){//, uint8_t *debug_data_buff, uint8_t debug_size){
+  int rc;
+  frame_t frame;
+  frame_header_t header;
+  goaway_payload_t goaway_pl;
+  uint8_t additional_debug_data[st->h2s.debug_size];
+  rc = create_goaway_frame(&header, &goaway_pl, additional_debug_data, st->h2s.last_open_stream_id, error_code, st->h2s.debug_data_buffer, st->h2s.debug_size);
+  if(rc < 0){
+    ERROR("Error creating GOAWAY frame");
+    return -1;
+  }
+  frame.frame_header = &header;
+  frame.payload = (void*)&goaway_pl;
+
+  uint8_t buff_bytes[HTTP2_MAX_BUFFER_SIZE];
+  int bytes_size = frame_to_bytes(&frame, buff_bytes);
+  rc = http_write(st,buff_bytes,bytes_size);
+  INFO("Sending GOAWAY");
+
+  if(rc != bytes_size){
+    ERROR("Error writting goaway frame. INTERNAL ERROR");
+    return -1;
+  }
+  st->h2s.sent_goaway = 1;
+  return 0;
+
+}
+
+void shutdown_connection(hstates_t *st){
+  st->connection_state = 0;
+}
+
+/*
+* Function: send_connection_error
+* Send a connection error to endpoint with a specified error code. It implements
+* the behaviour suggested in RFC 7540, secion 5.4.1
+* Input: -> st: hstates_t pointer where connetion variables are stored
+*        -> error_code: error code that will be used to shutdown the connection
+* Output: void
+*/
+
+void send_connection_error(hstates_t *st, uint32_t error_code){
+  int rc = send_goaway(st, error_code);
+  if(rc < 0){
+    WARN("Error sending GOAWAY frame to endpoint.");
+  }
+  shutdown_connection(st);
+  //TODO: communicate error code to HTTP?
+}
+
 /*
 * Function: update_settings_table
 * Updates the specified table of settings with a given settings payload.
@@ -102,19 +166,19 @@ int update_settings_table(settings_payload_t *spl, uint8_t place, hstates_t *st)
         switch(id){
           case ENABLE_PUSH:
               if(value != 0 && value != 1){
-                send_connection_error(HTTP2_PROTOCOL_ERROR);
+                send_connection_error(st, HTTP2_PROTOCOL_ERROR);
                 return -1;
               }
               break;
           case INITIAL_WINDOW_SIZE:
               if(value > 2147483647){
-                send_connection_error(HTTP2_FLOW_CONTROL_ERROR);
+                send_connection_error(st, HTTP2_FLOW_CONTROL_ERROR);
                 return -1;
               }
               break;
           case MAX_FRAME_SIZE:
               if(value > 16777215 || value < 16384){
-                send_connection_error(HTTP2_PROTOCOL_ERROR);
+                send_connection_error(st, HTTP2_PROTOCOL_ERROR);
                 return -1;
               }
               break;
@@ -254,70 +318,6 @@ int handle_settings_payload(settings_payload_t *spl, hstates_t *st){
         return -1;
     }
 }
-
-/*
-* Function: send_goaway
-* Given an hstates_t struct and a valid error code, sends a GOAWAY FRAME to endpoint.
-* If additional debug data (opaque data) is included it must be written on the
-* given hstates_t.h2s.debug_data_buffer buffer with its corresponding size on the
-* hstates_t.h2s.debug_size variable.
-* IMPORTANT: RFC 7540 section 6.8 indicates that a gracefully shutdown should have
-* an 2^31-1 value on the last_stream_id field and wait at least one RTT before sending
-* a goaway frame with the last stream id. This implementation doesn't.
-* Input: ->st: pointer to hstates_t struct where connection variables are stored
-*        ->error_code: error code for GOAWAY FRAME (RFC 7540 section 7)
-* Output: 0 if no errors were found, -1 if not
-*/
-int send_goaway(hstates_t *st, uint32_t error_code){//, uint8_t *debug_data_buff, uint8_t debug_size){
-  int rc;
-  frame_t frame;
-  frame_header_t header;
-  goaway_payload_t goaway_pl;
-  uint8_t additional_debug_data[st->h2s.debug_size];
-  rc = create_goaway_frame(&header, &goaway_pl, additional_debug_data, st->h2s.last_open_stream_id, error_code, st->h2s.debug_data_buffer, st->h2s.debug_size);
-  if(rc < 0){
-    ERROR("Error creating GOAWAY frame");
-    return -1;
-  }
-  frame.frame_header = &header;
-  frame.payload = (void*)&goaway_pl;
-
-  uint8_t buff_bytes[HTTP2_MAX_BUFFER_SIZE];
-  int bytes_size = frame_to_bytes(&frame, buff_bytes);
-  rc = http_write(st,buff_bytes,bytes_size);
-  INFO("Sending GOAWAY");
-
-  if(rc != bytes_size){
-    ERROR("Error writting goaway frame. INTERNAL ERROR");
-    return -1;
-  }
-  st->h2s.sent_goaway = 1;
-  return 0;
-
-}
-
-void shutdown_connection(hstates_t *st){
-  st->connection_state = 0;
-}
-
-/*
-* Function: send_connection_error
-* Send a connection error to endpoint with a specified error code. It implements
-* the behaviour suggested in RFC 7540, secion 5.4.1
-* Input: -> st: hstates_t pointer where connetion variables are stored
-*        -> error_code: error code that will be used to shutdown the connection
-* Output: void
-*/
-
-void send_connection_error(hstates_t *st, uint32_t error_code){
-  int rc = send_goaway(st, error_code);
-  if(rc < 0){
-    WARN("Error sending GOAWAY frame to endpoint.");
-  }
-  shutdown_connection(st);
-  //TODO: communicate error code to HTTP?
-}
-
 
 /*
 * Function: handle_goaway_payload
