80a83039eb2965c53fc015638844b254c82241f5
Pablo Aliaga <pablooaliaga@gmail.com>
2019-09-23 15:51:56 -0300

    Temporary change to utils

--- src/hpack/hpack_utils.h
+++ src/hpack/hpack_utils.h
@@ -2,7 +2,6 @@
 #define TWO_HPACK_UTILS_H
 
 #include <stdint.h>             /* for int8_t, uint8_t, uint32_t, uint16_t*/
-#include "hpack_tables.h"       /* for hpack_dynamic_table */
 #include "headers.h"            /* for MAX_HEADER_SETTINGS */
 
 //TODO move macro to conf file
@@ -13,6 +12,18 @@
 #else
 #define HPACK_MAXIMUM_INTEGER_SIZE (4096)
 #endif
+#ifdef HPACK_CONF_INCLUDE_DYNAMIC_TABLE
+#define HPACK_INCLUDE_DYNAMIC_TABLE (HPACK_CONF_INCLUDE_DYNAMIC_TABLE)
+#endif
+
+#ifdef HPACK_CONF_MAX_DYNAMIC_TABLE_SIZE
+#define HPACK_MAX_DYNAMIC_TABLE_SIZE (HPACK_CONF_MAX_DYNAMIC_TABLE_SIZE)
+#else
+#define HPACK_MAX_DYNAMIC_TABLE_SIZE (4096)
+#endif
+
+#define HPACK_INCLUDE_DYNAMIC_TABLE (1)
+
 typedef enum {
     INDEXED_HEADER_FIELD                            = (uint8_t) 128,
     LITERAL_HEADER_FIELD_WITH_INCREMENTAL_INDEXING  = (uint8_t) 64,
@@ -34,16 +45,42 @@ typedef struct {
     hpack_preamble_t preamble;
 } hpack_encoded_header_t;
 
+//typedefs for dinamic
+//size: 10 bytes in params + size of buffer
+typedef
+    #ifdef HPACK_INCLUDE_DYNAMIC_TABLE
+    struct hpack_dynamic_table {
+    uint16_t max_size;
+    uint16_t first;
+    uint16_t next;
+    uint16_t actual_size;
+    uint16_t n_entries;
+    char buffer[HPACK_MAX_DYNAMIC_TABLE_SIZE];
+}
+    #else
+    char
+    #endif
+hpack_dynamic_table_t;
+
 /* Hpack Struct: Hpack states
     It contains all the required structs or fields for hpack functionality
  */
 typedef struct {
+    #ifdef HPACK_INCLUDE_DYNAMIC_TABLE
     hpack_dynamic_table_t dynamic_table;
+    #else
+    char dynamic_table;
+    #endif
     hpack_encoded_header_t encoded_header;
     char tmp_name[MAX_HEADER_NAME_LEN];
     char tmp_value[MAX_HEADER_VALUE_LEN];
     uint32_t settings_max_table_size;
 } hpack_states_t;
+
+typedef enum {
+    PROTOCOL_ERROR  = (int8_t) -1,
+    INTERNAL_ERROR  = (int8_t) -2
+} hpack_error_t;
 
 
 uint32_t hpack_utils_read_bits_from_bytes(uint16_t current_bit_pointer, uint8_t number_of_bits_to_read, uint8_t *buffer);
