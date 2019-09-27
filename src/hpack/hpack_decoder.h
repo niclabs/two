3a748f38d08a5938e2d5f8c6865f9bc72bf43471
Pablo Aliaga <pablooaliaga@gmail.com>
2019-09-27 14:54:29 -0300

    Deleted unused commented functions in hpack_decode

--- src/hpack/hpack_decoder.h
+++ src/hpack/hpack_decoder.h
@@ -10,6 +10,4 @@
 
 int hpack_decoder_decode_header_block(hpack_states_t *states, uint8_t *header_block, uint8_t header_block_size, headers_t *headers);
 
-//int hpack_decoder_decode_header_block_from_table(hpack_dynamic_table_t *dynamic_table, uint8_t *header_block, uint8_t header_block_size, headers_t *headers);
-
 #endif //TWO_HPACK_DECODER_H
