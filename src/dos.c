203f1cef54409e199c7e749878fa7d665c71be9b
Javiera Alegría <javi@niclabs.cl>
2019-09-30 16:05:41 -0300

    Base for "dos" layer done

---
+++ src/dos.c
@@ -0,0 +1,13 @@
+/*
+   This API contains the "dos" methods to be used by
+   HTTP/2
+ */
+
+#include <errno.h>
+#include <strings.h>
+#include <stdio.h>
+
+//#define LOG_LEVEL (LOG_LEVEL_DEBUG)
+
+#include "dos.h"
+#include "logging.h"
---
+++ src/dos.h
@@ -0,0 +1,10 @@
+
+#ifndef DOS_H
+#define DOS_H
+
+#include <stdint.h>
+
+#include "resource_manager.h"
+
+
+#endif /* DOS_H */
