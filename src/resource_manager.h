d88d6a43312155976e09e6b49722d3cdfee028db
Javiera Alegría <javi@niclabs.cl>
2019-10-02 14:38:23 -0300

    Updated description of resource manager's API

--- src/resource_manager.h
+++ src/resource_manager.h
@@ -71,16 +71,32 @@ typedef struct {
 ***********************************************/
 
 /**
- * Generate a respnse from server to each client request
+ * Generate a response from server to each client request
  *
  * @param data_buff data structure
  * @param headers_buff headers data structure
  * @return 0 if ok -1 if an error ocurred
  */
-int http_server_response(data_t *data_buff, headers_t *headers_buff);
+int res_manager_server_response(data_t *data_buff, headers_t *headers_buff);
 
-
-int http_server_register_resource(resource_list_t *res_list, char *method, char *path, http_resource_handler_t handler);
+/**
+ * Set callback to handle an http resource
+ *
+ * A resource is composed by a method and a path
+ *
+ * A path consists of a sequence of path segments separated by a slash
+ * ("/") character.  A path is always defined for a URI, though the
+ * defined path may be empty (zero length)
+ * (More info in https://tools.ietf.org/html/rfc3986#section-3.3)
+ *
+ * For this function, the path must start with a '/'.
+ *
+ * Attempting to define a malformed path, or a path for an unsupported method
+ * will result in an error return
+ *
+ * @return 0 if ok, -1 if error
+ */
+int res_manager_server_register_resource(resource_list_t *res_list, char *method, char *path, http_resource_handler_t handler);
 
 
 #endif /* RESOURCE_MANAGER_H */
