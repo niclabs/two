feccb213d5deb68e464d2595aad84b3a9991362b
Gedoix <diegoortegop@hotmail.com>
2019-10-07 15:21:35 -0300

    Added documentation for the new callback struct wrapper

--- src/config.h
+++ src/config.h
@@ -2,6 +2,13 @@
 #define CONFIG_H
 #endif
 
+/*
+* Signature of a callback, wrapped in a stuct
+*
+* buf_in:       circular buffer, net writes into it
+* buf_out:      circular buffer, net reads from it
+* state:        net allocates memory for this, but doesn't touch it. inits to 0
+*/
 typedef struct CALLBACK {
   struct CALLBACK (* func)(cbuf_t* buf_in, cbuf_t* buf_out, void* state);
   void* debug_info; // just in case
