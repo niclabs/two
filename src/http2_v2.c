1c7e58533570f6d0f478282f66bd09a24b742c1c
Valeria Guidotti <vale@niclabs.cl>
2019-10-09 15:00:51 -0300

    Add check_incoming_goaway_condition

--- src/http2_v2.c
+++ src/http2_v2.c
@@ -308,6 +308,22 @@ int check_incoming_settings_condition(cbuf_t *buf_out, h2states_t *h2s)
   }
 }
 
+int check_incoming_goaway_condition(cbuf_t *buf_out, h2states_t *h2s)
+{
+  if(h2s->header.stream_id != 0x0){
+    ERROR("GOAWAY doesnt have STREAM ID 0. PROTOCOL ERROR");
+    send_connection_error(buf_out, HTTP2_PROTOCOL_ERROR, h2s);
+    return -1;
+  }
+  if(h2s->header.length > read_setting_from(h2s, LOCAL, MAX_FRAME_SIZE)){
+    ERROR("GOAWAY payload bigger than allowed. MAX_FRAME_SIZE ERROR");
+    send_connection_error(buf_out, HTTP2_FRAME_SIZE_ERROR, h2s);
+    return -1;
+  }
+  return 0;
+}
+
+
 int check_incoming_condition(cbuf_t *buf_out, h2states_t *h2s)
 {
     int rc;
