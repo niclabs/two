18ec1b069051a6cb2fa836ab0bc313c945f283cb
Javiera Alegría <javi@niclabs.cl>
2019-10-15 17:04:03 -0300

    Created tests for http_server_response function

--- tests/test_http.c
+++ tests/test_http.c
@@ -13,6 +13,10 @@
 
 #include <errno.h>
 
+#ifndef MIN
+#define MIN(n, m)   (((n) < (m)) ? (n) : (m))
+#endif
+
 /*Import of functions not declared in http.h */
 extern uint32_t get_data(uint8_t *data_in_buff, uint32_t data_in_buff_size, uint8_t *data_buffer, size_t size);
 extern int set_data(uint8_t *data_buff, uint32_t *data_buff_size, uint8_t *data, int data_size);
@@ -172,6 +176,63 @@ void test_do_request_fail_resource_handler_get(void)
 }
 
 
+void test_http_server_response_success(void)
+{
+    // Set auxiliary functions response
+    char *returnVals[2] = { "GET", "/index" };
+    SET_RETURN_SEQ(headers_get, returnVals, 2);
+    resource_handler_get_fake.return_val = &send_hello;
+
+    // Create function parameters
+    uint8_t data[10];
+    uint32_t data_size = (uint32_t)sizeof(data);
+    headers_t headers_buff[1];
+
+    // Perform response
+    int res = http_server_response((uint8_t *)&data, &data_size, (headers_t *)&headers_buff);
+
+    // Check that the auxiliary function have been called twice
+    TEST_ASSERT_EQUAL(2, headers_get_fake.call_count);
+
+    // Return value should be 0
+    TEST_ASSERT_EQUAL(0, res);
+}
+
+
+void test_http_server_response_fail_method_not_supported(void)
+{
+    // Set auxiliary functions response
+    char *returnVals[2] = { "POST", "/index" };
+
+    SET_RETURN_SEQ(headers_get, returnVals, 2);
+    resource_handler_get_fake.return_val = &send_hello;
+    headers_set_fake.custom_fake = headers_set_custom_fake;
+
+    // Create function parameters
+    uint8_t data[10];
+    uint32_t data_size = (uint32_t)sizeof(data);
+    headers_t headers_buff[1];
+
+    // Perform response
+    int res = http_server_response((uint8_t *)&data, &data_size, (headers_t *)&headers_buff);
+
+    // Check that the auxiliary function have been called twice
+    TEST_ASSERT_EQUAL(2, headers_get_fake.call_count);
+    TEST_ASSERT_EQUAL(1, headers_set_fake.call_count);
+
+    // Check that headers_set was called with the correct parameters
+    TEST_ASSERT_EQUAL_STRING("501", header_value);
+
+    // Check if the data and data_size have the correct content
+    uint32_t final_data_size = MIN((uint32_t)sizeof(data), (uint32_t)strlen("Not Implemented"));
+    TEST_ASSERT_EQUAL( final_data_size, data_size);
+    TEST_ASSERT_EQUAL( 0, memcmp(&data, (uint8_t *)"Not Implemented", 10));
+
+    // Return value should be 0
+    TEST_ASSERT_EQUAL(0, res);
+}
+
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -183,6 +244,9 @@ int main(void)
 
     UNIT_TEST(test_do_request_success);
     UNIT_TEST(test_do_request_fail_resource_handler_get);
+
+    UNIT_TEST(test_http_server_response_success);
+    UNIT_TEST(test_http_server_response_fail_method_not_supported);
 
     return UNITY_END();
 }
