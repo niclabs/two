31c5e2cabe8062c274e065ddf84b66b486ff81ed
Javiera Alegría <javi@niclabs.cl>
2019-10-17 16:54:07 -0300

    Created new server example

--- examples/server/server.c
+++ examples/server/server.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 #include <signal.h>
 
-#include "http.h"
+#include "dos.h"
 #include "logging.h"
 
-hstates_t http_server_state;
 
 void cleanup(int signal)
 {
     (void)signal;
 
-    PRINTF("Ctrl-C received. Terminating");
-    http_client_disconnect(&http_server_state);
-    http_server_destroy(&http_server_state);
+    PRINTF("Ctrl-C received");
 }
 
 int send_text(char * method, char * uri, uint8_t * response, int maxlen)
 {
     (void)method;
     (void)uri;
-    (void)maxlen;
+    if (maxlen < 16){
+      memcpy(response, "", 0);
+      return 0;
+    }
     memcpy(response, "hello world!!!!", 16);
     return 16;
 }
@@ -39,11 +39,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int rc = http_server_create(&http_server_state, port);
-
-    if (rc < 0) {
-        ERROR("in init server");
-    }
     else {
         rc = http_server_register_resource(&http_server_state, "GET", "/index", &send_text);
         if (rc < 0) {
@@ -51,7 +46,7 @@ int main(int argc, char **argv)
         }
         else {
 
-            rc = http_server_start(&http_server_state);
+            int rc = two_server_start(port);
             if (rc < 0) {
                 ERROR("in start server");
             }
