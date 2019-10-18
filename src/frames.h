8479cc77df29cfc6e2f0a9f7b5e13b655075685f
Gabriel Norambuena <gabo.norambuena@gmail.com>
2019-10-18 16:06:20 -0300

    frame update

--- src/frames.h
+++ src/frames.h
@@ -186,7 +186,6 @@ int compress_headers(headers_t *headers_out,  uint8_t *compressed_headers, hpack
 /*Data frame methods*/
 int create_data_frame(frame_header_t *frame_header, data_payload_t *data_payload, uint8_t *data, uint8_t *data_to_send, int length, uint32_t stream_id);
 int data_payload_to_bytes(frame_header_t *frame_header, void *payload, uint8_t *byte_array);
-int read_data_payload(frame_header_t *frame_header, void *payload, uint8_t *bytes);
 //int read_data_payload(frame_header_t *frame_header, void *payload, uint8_t *bytes);
 
 
