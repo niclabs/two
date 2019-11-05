57de2e599586ff468bedf7f6f67556677b8bde0e
Gedoix <diegoortegop@hotmail.com>
2019-11-05 11:50:53 -0300

    Recovered old sock implementation

---
+++ src/sock.c
@@ -0,0 +1,220 @@
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/types.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "sock.h"
+#include "logging.h"
+
+/* From listen(2): The backlog argument defines the maximum length to which the
+ * queue of pending connections for sockfd may grow. */
+#define SOCK_LISTEN_BACKLOG 1
+
+int sock_create(sock_t *sock)
+{
+    if (sock == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    sock->socket = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sock->socket < 0) {
+        sock->state = SOCK_CLOSED;
+        return -1;
+    }
+
+    sock->state = SOCK_OPENED;
+    return 0;
+}
+
+int sock_listen(sock_t *server, uint16_t port)
+{
+    if ((server == NULL) || (server->state != SOCK_OPENED)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    /* Struct sockaddr_in6 needed for binding. Family defined for ipv6. */
+    struct sockaddr_in6 sin6;
+    sin6.sin6_family = AF_INET6;
+    sin6.sin6_port = htons(port);
+    sin6.sin6_addr = in6addr_any;
+    if (bind(server->socket, (struct sockaddr *)&sin6, sizeof(sin6)) < 0) {
+        return -1;
+    }
+
+    if (listen(server->socket, SOCK_LISTEN_BACKLOG) < 0) {
+        return -1;
+    }
+
+    server->state = SOCK_LISTENING;
+    return 0;
+}
+
+int sock_accept(sock_t *server, sock_t *client)
+{
+    if ((server == NULL) || (server->state != SOCK_LISTENING)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    int clifd = accept(server->socket, NULL, NULL);
+    if (clifd < 0) {
+        return -1;
+    }
+
+    // Only struct values if client is not null
+    if (client != NULL) {
+        client->socket = clifd;
+        client->state = SOCK_CONNECTED;
+    }
+    return 0;
+}
+
+int sock_connect(sock_t *client, char *addr, uint16_t port)
+{
+    if (client == NULL || (client->state != SOCK_OPENED)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (addr == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    // convert address string to a socket address
+    struct in6_addr address;
+    int pton_res = inet_pton(AF_INET6, addr, &address);
+
+    // string is not valid INET6 address
+    if (pton_res == 0) {
+        errno = EINVAL;
+        ERROR("'%s' is not a valid IPv6 address", addr);
+        return -1;
+    }
+
+    // AF_INET6 is not supported
+    if (pton_res < 0) {
+        ERROR("The system does not support IPv6 addresses");
+        return -1;
+    }
+
+    struct sockaddr_in6 sin6;
+    sin6.sin6_port = htons(port);
+    sin6.sin6_family = AF_INET6;
+    sin6.sin6_addr = address;
+    if (connect(client->socket, (struct sockaddr *)&sin6, sizeof(sin6)) < 0) {
+        ERROR("Failed to connect to [%s]:%d", addr, port);
+        return -1;
+    }
+
+    client->state = SOCK_CONNECTED;
+    return 0;
+}
+
+int sock_read(sock_t *sock, char *buf, int len, int timeout)
+{
+    if (sock == NULL || (sock->state != SOCK_CONNECTED)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (buf == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+
+    // set timeout
+    if (timeout > 0) {
+        // use select to wait for sock to have reading data
+        fd_set read_fds;
+        FD_ZERO(&read_fds);             // prepare fd_set
+        FD_SET(sock->socket, &read_fds);    // add sock->socket to fd_set
+
+        struct timeval tv;
+        tv.tv_sec = timeout;
+        tv.tv_usec = 0;
+
+        int res = select(sock->socket + 1, &read_fds, NULL, NULL, &tv);
+        if (res == -1) {
+            return -1;
+        }
+        else if (res != 1) { // timeout reached
+            errno = ETIME;
+            return -1;
+        }
+    }
+
+    // read from socket
+    return read(sock->socket, buf, len);
+}
+
+int sock_write(sock_t *sock, char *buf, int len)
+{
+    if (sock == NULL || sock->state != SOCK_CONNECTED) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (buf == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ssize_t bytes_written;
+    ssize_t bytes_written_total = 0;
+    const char *p = buf;
+
+    /**
+     * Note that a successful write() may transfer fewer than count bytes.
+     * Is expected that when all bytes are transfered, there's no length
+     * left of the message and/or there's no information left in the
+     * buffer.
+     */
+    while (len > 0) {
+        bytes_written = write(sock->socket, p, len);
+        if (bytes_written < 0) {
+            return -1;
+        }
+        p += bytes_written;
+        len -= bytes_written;
+        bytes_written_total += bytes_written;
+    }
+    return bytes_written_total;
+}
+
+int sock_destroy(sock_t *sock)
+{
+	if (sock == NULL || sock->state != SOCK_CONNECTED) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (sock->state == SOCK_CLOSED) {
+		errno = EBADF;
+		return -1;
+	}
+
+	struct linger sl;
+	sl.l_onoff = 1;  /* non-zero value enables linger option in kernel */
+	sl.l_linger = 0; /* timeout interval in seconds */
+
+	// Configure socket to wait for data to send before closing
+	setsockopt(sock->socket, SOL_SOCKET, SO_LINGER, &sl, sizeof(sl));
+
+	if (close(sock->socket) < 0) {
+		return -1;
+	}
+
+	sock->socket = -1;
+	sock->state = SOCK_CLOSED;
+	return 0;
+}
---
+++ src/sock.h
@@ -0,0 +1,173 @@
+/**
+ * TCP Socket API abstraction
+ * 
+ */
+
+#ifndef SOCK_H
+#define SOCK_H
+
+#include <stdint.h>
+
+#ifdef WITH_CONTIKI
+#include "sys/process.h"
+
+#ifdef SOCK_CONF_BUFFER_SIZE
+#define SOCK_BUFFER_SIZE (SOCK_CONF_BUFFER_SIZE)
+#else 
+#define SOCK_BUFFER_SIZE 128
+#endif
+
+#ifdef SOCK_CONF_MAX_SOCKETS
+#define SOCK_MAX_SOCKETS (SOCK_CONF_MAX_SOCKETS)
+#else
+#define SOCK_MAX_SOCKETS UIP_CONNS
+#endif
+
+#define PROCESS_SOCK_WAIT(sock, thread) PROCESS_PT_SPAWN(&(sock)->pt, thread)
+
+typedef struct sock_socket * sock_socket_t;
+#else
+typedef int sock_socket_t;
+#endif /* WITH_CONTIKI */
+
+
+/* Socket states */
+typedef enum {
+    SOCK_CLOSED,
+    SOCK_OPENED,
+    SOCK_LISTENING,
+    SOCK_CONNECTED
+} sock_state_t;
+
+/**
+ * Socket typedef
+ */
+typedef struct {
+    sock_socket_t socket;
+    int state;
+#ifdef WITH_CONTIKI
+    struct pt pt;
+    uint16_t port;
+#endif
+} sock_t;
+
+/**
+ * Initialize a new socket to act as either a server or a client
+ * 
+ * @param sock pointer to socket data structure (defined by the implementation)
+ * 
+ * @return  0   if socket was created succesfully
+ * @return -1   on error (errno will be set appropriately)
+ */
+int sock_create(sock_t * sock);
+
+/**
+ * Configure provided socket to act as a server in the specified port
+ * 
+ * @param   server  pointer to socket data structure (defined by the implementation)
+ * @param   port    port to listen for connections
+ * 
+ * @return  0   if setting socket to act as a server succeeds
+ * @return -1   on error (errno will be set appropriately)
+ */
+int sock_listen(sock_t * server, uint16_t port);
+
+/**
+ * Wait for connections on server socket. Server socket must be set to listen by calling
+ * sock_listen() previously, otherwise an error will be returned.
+ *
+ * In contiki, the method does not block and returns 0 only if there is a connection already
+ * in order for the process to wait for a client, the macro PROCES_SOCk_WAIT_CLIENT must be used
+ * before. 
+ * 
+ * @param   server      pointer to server socket data structure (defined by the implementation)
+ * @param   client      pointer to client socket data structure (it will be initialized by the function)
+ * 
+ * @return   0      if a new client was successfully accepted
+ * @return  -1      on error (errno will be set appropriately)
+ */
+int sock_accept(sock_t * server, sock_t * client);
+
+/**
+ * Connect the client socket to the specified address and port. The socket must be initialized with
+ * sock_create() previously
+ * 
+ * @param   client  pointer to client socket data structure
+ * @param   address remote address to connect the socket to
+ * @param   port    remote port to connect to
+ * 
+ * @return   0      if a new connection was successfully established
+ * @return  -1      on error (errno will be set appropriately)
+ */
+int sock_connect(sock_t * client, char * addr, uint16_t port);
+
+/**
+ * Read len bytes from the socket into the specified buffer. The method will wait for timeout seconds
+ * if timeout is greater than zero
+ *
+ * In contiki, the method does not block and returns 0 only if there is a connection already
+ * in order for the process to wait for new data, the macro PROCES_SOCk_WAIT_DATA must be used
+ * before. 
+ * 
+ * @param   sock    pointer to socket data structure to read from
+ * @param   buf     buffer to read the data into
+ * @param   len     number of bytes to read
+ * @param   timeout timeout in seconds to wait for read before giving up, If set to zero, the method will wait 
+ *                  indefinitely or until there is data to read
+ * 
+ * @return   >0     number of bytes read
+ * @return   0      if connection was closed on the other side
+ * @return  -1      on error (errno will be set appropriately)
+ */
+int sock_read(sock_t * sock, char * buf, int len, int timeout);
+
+/**
+ * Write len bytes from the buffer into the socket. The method will attempt to write all bytes unless an error 
+ * ocurrs
+ * 
+ * @param   sock    pointer to socket data structure to read from
+ * @param   buf     buffer to read the data into
+ * @param   len     number of bytes to read
+ * 
+ * @return >0   number of bytes written
+ * @return 0    if connection was closed on the other side
+ * @return -1   on error (errno will be set appropriately)
+ */
+int sock_write(sock_t * sock, char * buf, int len);
+
+/**
+ * Close the connection and destroy the socket
+ * 
+ * @param   sock    pointer to socket data structure
+ * 
+ * @return  0   is connection was closed succesfully
+ * @return  -1  on error (errno will be set appropriately)
+ */
+int sock_destroy(sock_t * sock);
+
+#ifdef WITH_CONTIKI
+/**
+ * Wait for the underlying socket to connect, either 
+ * by a new client arriving (in the case of a server sock)
+ * or by the remote server becoming available
+ *
+ * @param sock socket configured and listening
+ */ 
+PT_THREAD(sock_wait_connection(sock_t * sock));
+
+/**
+ * Wait for new data to arrive on the provided client sock
+ *
+ * @param sock connected client sock
+ **/
+PT_THREAD(sock_wait_data(sock_t * sock));
+
+/**
+ * Process sock macros
+ */
+#define PROCESS_SOCK_WAIT_CONNECTION(sock) PROCESS_SOCK_WAIT(sock, sock_wait_connection(sock))
+#define PROCESS_SOCK_WAIT_DATA(sock) PROCESS_SOCK_WAIT(sock, sock_wait_data(sock))
+#endif
+
+
+#endif /* SOCK_H */
