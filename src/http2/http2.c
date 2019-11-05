ffd03bd1348ad18cf6a8a996efe392d0404c7f71
Valeria Guidotti <vale@niclabs.cl>
2019-11-05 11:31:38 -0300

    Change last callback return in receive_payload

--- src/http2/http2.c
+++ src/http2/http2.c
@@ -143,15 +143,14 @@ callback_t receive_payload(cbuf_t *buf_in, cbuf_t *buf_out, void *state)
         return null_callback();
     }
     rc = handle_payload(buff_read_payload, buf_out, h2s);
-    // todo: read_type_payload from buff_read_payload
 
-    // placeholder
-    callback_t ret_null = { NULL, NULL };
-    return ret_null;
     if(rc < 0){
       ERROR("Error was found while handling payload");
       return null_callback();
     }
+
+    callback_t ret = { receive_header, NULL };
+    return ret;
 }
 
 
