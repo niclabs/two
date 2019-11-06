a56a35a22a0d24e03d1aa200587f9d849d3f45da
Valeria Guidotti <vale@niclabs.cl>
2019-11-06 14:24:30 -0300

    Add fake functions and setup for test_http2_check

--- tests/test_http2_check.c
+++ tests/test_http2_check.c
@@ -6,17 +6,32 @@
 #include "frames/common.h"  // for frame_header_t
 #include "cbuf.h"           // for cbuf
 
-// Include header definitions for file to test 
-// e.g #include "sock.h"
+/* Import of functions not declared in http2/check.h */
+extern uint32_t read_setting_from(h2states_t *st, uint8_t place, uint8_t param);
+extern void send_connection_error(cbuf_t *buf_out, uint32_t error_code, h2states_t *h2s);
 
-void test_example(void) {
-    TEST_FAIL();
+DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(uint32_t, read_setting_from, h2states_t *, uint8_t, uint8_t);
+FAKE_VOID_FUNC(send_connection_error, cbuf_t *, uint32_t, h2states_t *);
+
+#define FFF_FAKES_LIST(FAKE)            \
+    FAKE(read_setting_from)             \
+    FAKE(send_connection_error)         \
+
+void setUp(void)
+{
+    /* Register resets */
+    FFF_FAKES_LIST(RESET_FAKE);
+    /* reset common FFF internal structures */
+    FFF_RESET_HISTORY();
+
+}
 }
 
 int main(void)
 {
     UNIT_TESTS_BEGIN();
-    
+
     // Call tests here
     UNIT_TEST(test_example);
 
