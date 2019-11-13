06e4da29637e48020133c10f3e4edeafa20ad011
Sebastián Cifuentes <seba@niclabs.cl>
2019-11-13 16:38:14 -0300

    add handle_headers test incomplete

--- tests/test_http2_handle.c
+++ tests/test_http2_handle.c
@@ -5,18 +5,23 @@
 #include "http2/handle.h"
 #include "frames/common.h"  // for frame_header_t
 #include "frames/data_frame.h"  // for data_payload_t
+#include "frames/headers_frame.h"  // for headers_payload_t
 #include "cbuf.h"           // for cbuf
 
 DEFINE_FFF_GLOBALS;
 FAKE_VALUE_FUNC(int, flow_control_receive_data, h2states_t *, uint32_t);
 FAKE_VOID_FUNC(send_connection_error, cbuf_t *, uint32_t, h2states_t *);
-FAKE_VOID_FUNC(buffer_copy, uint8_t*, uint8_t*, int);
+FAKE_VALUE_FUNC(int, buffer_copy, uint8_t*, uint8_t*, int);
 FAKE_VALUE_FUNC(int, is_flag_set, uint8_t, uint8_t);
 FAKE_VALUE_FUNC(int, change_stream_state_end_stream_flag, uint8_t, cbuf_t *, h2states_t *);
 FAKE_VALUE_FUNC(int, http_server_response, uint8_t *, uint32_t *, headers_t *);
 FAKE_VALUE_FUNC(int, send_response, cbuf_t *, h2states_t *);
 FAKE_VALUE_FUNC(int, headers_validate, headers_t*);
 FAKE_VALUE_FUNC(char *, headers_get, headers_t *, const char *);
+FAKE_VALUE_FUNC(uint32_t, get_header_block_fragment_size, frame_header_t *, headers_payload_t *);
+FAKE_VALUE_FUNC(int, receive_header_block, uint8_t *, int, headers_t *, hpack_states_t *);
+FAKE_VALUE_FUNC(uint32_t, headers_get_header_list_size, headers_t*);
+FAKE_VALUE_FUNC(uint32_t, read_setting_from, h2states_t *, uint8_t, uint8_t);
 
 #define FFF_FAKES_LIST(FAKE)                                    \
     FAKE(flow_control_receive_data)                             \
@@ -43,6 +48,7 @@ void test_handle_data_payload_no_flags(void) {
     h2s.data.size = 0;
     // Fake settings
     flow_control_receive_data_fake.return_val = 0;
+    buffer_copy_fake.return_val = 10;
     is_flag_set_fake.return_val = 0;
     int rc = handle_data_payload(&head, &dpl, &bout, &h2s);
     TEST_ASSERT_EQUAL_MESSAGE(rc, 0, "Method should return 0. No errors were set");
@@ -55,16 +61,6 @@ void test_handle_data_payload_multi_data(void) {
     data_payload_t dpl;
     cbuf_t bout;
     h2states_t h2s;
-    // h2s.headers.headers = h2s.headers_buf;
-    // h2s.headers.count = 0;
-    // h2s.headers.maxlen = 200;
-    // strncpy(h2s.headers.headers[0].name, ":method", 20);
-    // strncpy(h2s.headers.headers[0].value, "value", 20);
-    // strncpy(h2s.headers.headers[1].name, ":scheme", 20);
-    // strncpy(h2s.headers.headers[1].value, "value", 20);
-    // strncpy(h2s.headers.headers[2].name, ":path", 20);
-    // strncpy(h2s.headers.headers[2].value, "value", 20);
-    // h2s.headers.count = 3;
     h2s.data.size = 0;
     // Fake settings
     flow_control_receive_data_fake.return_val = 0;
@@ -73,7 +69,6 @@ void test_handle_data_payload_multi_data(void) {
     http_server_response_fake.return_val = 0;
     send_response_fake.return_val = 0;
     headers_get_fake.return_val = "value";
-    //cbuf_pop_fake.custom_fake = cbuf_pop_preface_fake;
     int rc = handle_data_payload(&head, &dpl, &bout, &h2s);
     TEST_ASSERT_EQUAL_MESSAGE(rc, 0, "Method should return 0. No errors were set");
     rc = handle_data_payload(&head, &dpl, &bout, &h2s);
@@ -83,6 +78,15 @@ void test_handle_data_payload_multi_data(void) {
     TEST_ASSERT_EQUAL_MESSAGE(h2s.data.size, 3*head.length, "Data size must be equal to header payload's length");
 }
 
+void test_handle_headers_payload(void){
+    frame_header_t head;
+    headers_payload_t hpl;
+    cbuf_t bout;
+    h2states_t h2s;
+    int rc = handle_headers_payload(&head, &hpl, &bout, &h2s);
+    TEST_ASSERT_EQUAL_MESSAGE(0, rc, "Method should return 0. No errors were set");
+}
+
 int main(void)
 {
     UNIT_TESTS_BEGIN();
@@ -90,6 +94,6 @@ int main(void)
     // Call tests here
     UNIT_TEST(test_handle_data_payload_no_flags);
     UNIT_TEST(test_handle_data_payload_multi_data);
-
+    // UNIT_TEST(test_handle_headers_payload);
     return UNIT_TESTS_END();
 }
