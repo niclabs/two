b13f481a3c12457347eaff724056d99c1d855483
Valeria Guidotti <vale@niclabs.cl>
2019-11-13 14:33:39 -0300

    Define fake function set_flag

--- tests/test_http2_send.c
+++ tests/test_http2_send.c
@@ -6,15 +6,30 @@
 #include "frames/common.h"
 #include "cbuf.h"
 
+DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(uint8_t, set_flag, uint8_t, uint8_t);
 
-void test_example(void) {
+#define FFF_FAKES_LIST(FAKE)            \
+    FAKE(set_flag)                      \
+
+void setUp(void)
+{
+    /* Register resets */
+    FFF_FAKES_LIST(RESET_FAKE);
+    /* reset common FFF internal structures */
+    FFF_RESET_HISTORY();
+
+}
+
+void test_example(void)
+{
     TEST_FAIL();
 }
 
 int main(void)
 {
     UNIT_TESTS_BEGIN();
-    
+
     // Call tests here
     UNIT_TEST(test_example);
 
