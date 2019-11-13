de3e2fd2ea46d6a9788892d55edd1b2944b6a2cb
Valeria Guidotti <vale@niclabs.cl>
2019-11-13 16:23:05 -0300

    Add fake functions to test_http2_send

--- tests/test_http2_send.c
+++ tests/test_http2_send.c
@@ -7,10 +7,19 @@
 #include "cbuf.h"
 
 DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(int, cbuf_push, cbuf_t *, void *, int);
+FAKE_VALUE_FUNC(int, create_goaway_frame, frame_header_t *, goaway_payload_t *, uint8_t *, uint32_t, uint32_t,  uint8_t *, uint8_t);
+FAKE_VALUE_FUNC(int, frame_to_bytes, frame_t *, uint8_t *);
+FAKE_VALUE_FUNC(int, prepare_new_stream, h2states_t *);
 FAKE_VALUE_FUNC(uint8_t, set_flag, uint8_t, uint8_t);
 
 #define FFF_FAKES_LIST(FAKE)            \
+    FAKE(cbuf_push)                     \
+    FAKE(create_goaway_frame)           \
+    FAKE(frame_to_bytes)                \
+    FAKE(prepare_new_stream)            \
     FAKE(set_flag)                      \
+
 
 void setUp(void)
 {
