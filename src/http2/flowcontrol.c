551480c322d91e57a472cab3dafae5635a63e442
Valeria Guidotti <vale@niclabs.cl>
2019-11-14 15:07:09 -0300

    Refactor return to use enum in increase_window_used

--- src/http2/flowcontrol.c
+++ src/http2/flowcontrol.c
@@ -23,7 +23,7 @@ uint32_t get_window_available_size(h2_window_manager_t window_manager)
 int increase_window_used(h2_window_manager_t *window_manager, uint32_t data_size)
 {
     window_manager->window_used += data_size;
-    return 0;
+    return HTTP2_RC_NO_ERROR;
 }
 
 /*
