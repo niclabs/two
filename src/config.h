6cbe373876eb06f373ba5a52716d096635016697
Gedoix <diegoortegop@hotmail.com>
2019-11-20 13:04:08 -0300

    Removed optional logging from the repo

--- src/config.h
+++ src/config.h
@@ -6,12 +6,16 @@
 #define HTTP2_MAX_HBF_BUFFER 16384
 
 // Logging per module
+/* 
 #undef LOG_LEVEL_NET
 #define LOG_LEVEL_NET   (LOG_LEVEL_DEBUG)
 #undef LOG_LEVEL_FRAME
 #define LOG_LEVEL_FRAME (LOG_LEVEL_DEBUG)
 #undef LOG_LEVEL_HTTP2
 #define LOG_LEVEL_HTTP2 (LOG_LEVEL_DEBUG)
+#undef LOG_LEVEL_HPACK
+#define LOG_LEVEL_HPACK (LOG_LEVEL_DEBUG)
+ */
 
 /*
 * Signature of a callback, wrapped in a stuct
