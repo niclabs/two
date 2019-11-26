14d339f7b823e8e1ba2ae7522f5ae2c9a44717a5
Sebastián Cifuentes <seba@niclabs.cl>
2019-11-26 15:42:12 -0300

    Modified return type of read_setting_from in handle.c

--- src/http2/handle.c
+++ src/http2/handle.c
@@ -168,11 +168,7 @@ int handle_headers_payload(frame_header_t *header, headers_payload_t *hpl, cbuf_
             return (h2_ret_code_t)send_response(buf_out, h2s);
         }
         uint32_t header_list_size = headers_get_header_list_size(&h2s->headers);
-        int setting_read = read_setting_from(h2s, LOCAL, MAX_HEADER_LIST_SIZE);
-        if (setting_read < 0) {
-            //TODO send error if necessary, check return code
-            return HTTP2_RC_ERROR;
-        }
+        uint32_t setting_read = read_setting_from(h2s, LOCAL, MAX_HEADER_LIST_SIZE);
         uint32_t MAX_HEADER_LIST_SIZE_VALUE = (uint32_t)setting_read;
         if (header_list_size > MAX_HEADER_LIST_SIZE_VALUE) {
             ERROR("Header list size greater than max allowed. Send HTTP 431");
@@ -400,11 +396,7 @@ int handle_continuation_payload(frame_header_t *header, continuation_payload_t *
             return (h2_ret_code_t)send_response(buf_out, h2s);
         }
         uint32_t header_list_size = headers_get_header_list_size(&h2s->headers);
-        int setting_read = read_setting_from(h2s, LOCAL, MAX_HEADER_LIST_SIZE);
-        if (setting_read < 0) {
-            //TODO send error if necessary, check return code
-            return HTTP2_RC_ERROR;
-        }
+        uint32_t setting_read = read_setting_from(h2s, LOCAL, MAX_HEADER_LIST_SIZE);
         uint32_t MAX_HEADER_LIST_SIZE_VALUE = (uint32_t)setting_read;
         if (header_list_size > MAX_HEADER_LIST_SIZE_VALUE) {
             WARN("Header list size greater than max allowed. Send HTTP 431");
