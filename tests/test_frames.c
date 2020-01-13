7bb89796dd8dd45f2351fea88a0824b70e8dae4a
Felipe Lalanne <flalanne@niclabs.cl>
2020-01-13 12:23:14 -0300

    Add placeholder tests for frames and http2 v3

---
+++ tests/test_frames.c
@@ -0,0 +1,17 @@
+#include "unit.h"
+#include "frames.h"
+
+void setUp()
+{}
+
+void test_send_goaway_frame(void)
+{
+    TEST_FAIL();
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    UNIT_TEST(test_send_goaway_frame);
+    return UNITY_END();
+}
---
+++ tests/test_http2.c
@@ -0,0 +1,17 @@
+#include "unit.h"
+#include "http2.h"
+
+void setUp()
+{}
+
+void test_handle_frame(void)
+{
+    TEST_FAIL();
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    UNIT_TEST(test_handle_frame);
+    return UNITY_END();
+}
