7dd941e6d165cf416c9a1b12ad01da0813106cc8
Felipe Lalanne <flalanne@niclabs.cl>
2020-01-21 11:45:43 -0300

    Add comment to project-conf.h

--- examples/server/project-conf.h
+++ examples/server/project-conf.h
@@ -1,9 +1,13 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-// Contiki logs
-#define LOG_CONF_LEVEL_IPV6 LOG_LEVEL_NONE
+/* QUEUEBUF_CONF_NUM specifies the number of queue buffers. Queue
+   buffers are used throughout the Contiki netstack but the
+   configuration option can be tweaked to save memory. Performance can
+   suffer with a too low number of queue buffers though.
 
+   We reduce the queue buffer size to allow the two implementation to fit
+   in the .bss memory section. */
 #define QUEUEBUF_CONF_NUM 4
 
 #endif
