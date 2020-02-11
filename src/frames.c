d9672bf1c900b979b49d2e48790f0f8bd665360b
Felipe Lalanne <felipe@lalanne.cl>
2020-02-11 15:47:53 -0300

    Use static memory for frame encoding
    Prevent running out of stack when creating a header frame
    in embedded target

--- src/frames.c
+++ src/frames.c
@@ -12,6 +12,18 @@
 #define LOG_MODULE LOG_MODULE_FRAME
 #include "logging.h"
 
+#ifndef CONFIG_FRAME_MAX_SIZE
+#define FRAME_MAX_SIZE (HTTP2_SOCK_WRITE_SIZE)
+#elif (CONFIG_FRAME_MAX_SIZE) > (HTTP2_SOCK_WRITE_SIZE)
+#error "Max frame size cannot be larger than http2 write buffer size"
+#else
+#define FRAME_MAX_SIZE (CONFIG_FRAME_MAX_SIZE)
+#endif
+
+// reserve some static memory
+// to create frames
+static uint8_t frame_bytes[FRAME_MAX_SIZE];
+
 /*
  * Function: frame_header_to_bytes
  * Convert a frame header into an array of bytes
@@ -54,87 +66,86 @@ void frame_parse_header(frame_header_t *header, uint8_t *data, unsigned int size
 
 int send_ping_frame(event_sock_t *socket, uint8_t *opaque_data, int ack, event_write_cb cb)
 {
-    /*First we create the header*/
-    frame_header_t header;
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
 
-    //ping_payload_t ping_payload;
+    // Create the frame header
+    frame_header_t header;
     header.length = 8;
     header.type = FRAME_PING_TYPE;
-    header.flags = (uint8_t) (ack ? FRAME_FLAGS_ACK : 0);
+    header.flags = (uint8_t)(ack ? FRAME_FLAGS_ACK : 0);
     header.reserved = 0;
     header.stream_id = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + 8];     /*ping  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + 8);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
+    /*We put the payload on the reserved buffer */
+    memcpy(frame_bytes + frame_size, opaque_data, header.length);
+    frame_size += header.length;
 
-    /*We put the payload on the buffer*/
-    memcpy(response_bytes + size_bytes, opaque_data, header.length);
-    size_bytes += header.length;
-
-    // We write the ping to NET
-    return event_write(socket, size_bytes, response_bytes, cb);
+    // We write the ping to the network
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 int send_goaway_frame(event_sock_t *socket, uint32_t error_code, uint32_t last_open_stream_id, event_write_cb cb)
 {
-    frame_header_t header;
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
 
+    // Create the frame header
+    frame_header_t header;
     header.length = 8;
     header.type = FRAME_GOAWAY_TYPE;
     header.flags = 0;
     header.stream_id = 0;
     header.reserved = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*ping  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
     /*We put the payload on the buffer*/
-    uint32_31_to_byte_array(last_open_stream_id, response_bytes + size_bytes);
-    size_bytes += 4;
+    uint32_31_to_byte_array(last_open_stream_id, frame_bytes + frame_size);
+    frame_size += 4;
 
-    uint32_to_byte_array(error_code, response_bytes + size_bytes);
-    size_bytes += 4;
+    uint32_to_byte_array(error_code, frame_bytes + frame_size);
+    frame_size += 4;
 
-    return event_write(socket, size_bytes, response_bytes, cb);
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 int send_settings_frame(event_sock_t *socket, int ack, uint32_t settings_values[], event_write_cb cb)
 {
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
 
     uint8_t count = 6;
     uint16_t ids[] = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6 };
 
+    // Create the frame header
     frame_header_t header;
-
-    /*rc must be 0*/
     header.length = ack ? 0 : (6 * count);
-    header.type = FRAME_SETTINGS_TYPE;    //settings;
-    header.flags = (uint8_t) (ack ? FRAME_FLAGS_ACK : 0);
+    header.type = FRAME_SETTINGS_TYPE;
+    header.flags = (uint8_t)(ack ? FRAME_FLAGS_ACK : 0);
     header.reserved = 0x0;
     header.stream_id = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*settings  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
-
+    // write payload
     for (int i = 0; i < count && !ack; i++) {
         uint16_t identifier = ids[i];
-        uint16_to_byte_array(identifier, response_bytes + size_bytes);
-        size_bytes += 2;
+        uint16_to_byte_array(identifier, frame_bytes + frame_size);
+        frame_size += 2;
 
         uint32_t value = settings_values[i];
-        uint32_to_byte_array(value, response_bytes + size_bytes);
-        size_bytes += 4;
+        uint32_to_byte_array(value, frame_bytes + frame_size);
+        frame_size += 4;
     }
 
-    return event_write(socket, size_bytes, response_bytes, cb);
+    // write to socket
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 
@@ -145,99 +156,105 @@ int send_headers_frame(event_sock_t *socket,
                        uint8_t end_stream,
                        event_write_cb cb)
 {
-    uint8_t encoded_bytes[HTTP2_SOCK_WRITE_SIZE];
-    memset(encoded_bytes,0, HTTP2_SOCK_WRITE_SIZE);
-    int size = hpack_encode(dynamic_table, headers_list, encoded_bytes, HTTP2_SOCK_WRITE_SIZE);
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
 
-    if (size < 0) { //Error
-        return size;
+    // try to encode hpack into the buffer, leaving
+    // space for the frame header
+    int encoded_size = hpack_encode(dynamic_table, headers_list,
+                                    frame_bytes + 9,
+                                    FRAME_MAX_SIZE - 9);
+
+    if (encoded_size < 0) {
+        return encoded_size;
     }
-    frame_header_t header;
 
-    // We create the headers frame
-    header.length = size;
+    // Create the frame header
+    frame_header_t header;
+    header.length = encoded_size;
     header.type = FRAME_HEADERS_TYPE;
     header.flags = FRAME_FLAGS_END_HEADERS; // we never send continuation
-    header.flags |= (uint8_t) (end_stream ? FRAME_FLAGS_END_STREAM : 0x0);
+    header.flags |= (uint8_t)(end_stream ? FRAME_FLAGS_END_STREAM : 0x0);
     header.stream_id = stream_id;
     header.reserved = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*settings  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
+    frame_size += encoded_size;
 
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
-
-    /*Then we put the payload*/
-    memcpy(response_bytes + size_bytes, encoded_bytes, header.length);
-    size_bytes += header.length;
-
-    return event_write(socket, size_bytes, response_bytes, cb);
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 int send_window_update_frame(event_sock_t *socket, uint8_t window_size_increment, uint32_t stream_id, event_write_cb cb)
 {
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
+    
+    // Create the frame header
     frame_header_t header;
-
     header.stream_id = stream_id;
     header.type = FRAME_WINDOW_UPDATE_TYPE;
     header.length = 4;
     header.reserved = 0;
     header.flags = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*settings  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
-
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
     /*Then we put the payload*/
-    uint32_31_to_byte_array(window_size_increment, response_bytes + size_bytes);
-    size_bytes += header.length;
+    uint32_31_to_byte_array(window_size_increment, frame_bytes + frame_size);
+    frame_size += header.length;
 
-    return event_write(socket, size_bytes, response_bytes, cb);
+    // write to the socket
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 int send_rst_stream_frame(event_sock_t *socket, uint32_t error_code, uint32_t stream_id, event_write_cb cb)
 {
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
+    
+    // Create the frame header
     frame_header_t header;
-
     header.stream_id = stream_id;
     header.type = FRAME_RST_STREAM_TYPE;
     header.length = 4;
     header.flags = 0;
     header.reserved = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*settings  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
-
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
     /*Then we put the payload*/
-    uint32_to_byte_array(error_code, response_bytes + size_bytes);
-    size_bytes += header.length;
+    uint32_to_byte_array(error_code, frame_bytes + frame_size);
+    frame_size += header.length;
 
-    return event_write(socket, size_bytes, response_bytes, cb);
+    // write to the socket
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
 
 int send_data_frame(event_sock_t *socket, uint8_t *data, uint32_t size, uint32_t stream_id, uint8_t end_stream, event_write_cb cb)
 {
-    frame_header_t header;
+    // reset the reserved memory
+    memset(frame_bytes, 0, FRAME_MAX_SIZE);
 
+    // Create the frame header
+    // TODO: check if frame fits in frame_size before setting
+    // end stream flag?
+    frame_header_t header;
     header.length = size;
     header.type = FRAME_DATA_TYPE;
-    header.flags = (uint8_t) (end_stream ? FRAME_FLAGS_END_STREAM : 0x0);
+    header.flags = (uint8_t)(end_stream ? FRAME_FLAGS_END_STREAM : 0x0);
     header.stream_id = stream_id;
     header.reserved = 0;
 
-    /*Then we put it in a buffer*/
-    uint8_t response_bytes[9 + header.length];     /*settings  frame has a header and a payload of 8 bytes*/
-    memset(response_bytes, 0, 9 + header.length);
+    // copy header data into the beginning of the frame
+    int frame_size = frame_header_to_bytes(&header, frame_bytes);
 
-    int size_bytes = frame_header_to_bytes(&header, response_bytes);
-    memcpy(response_bytes + size_bytes, data, header.length);
-    size_bytes += header.length;
+    // copy data into the frame
+    memcpy(frame_bytes + frame_size, data, size);
+    frame_size += size;
 
-    return event_write(socket, size_bytes, response_bytes, cb);
+    // write to the socket
+    return event_write(socket, frame_size, frame_bytes, cb);
 }
