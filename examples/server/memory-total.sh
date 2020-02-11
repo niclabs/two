bc422f6465b76ace6c0026ad7b487efeff814bc3
Felipe Lalanne <felipe@lalanne.cl>
2020-02-11 15:54:11 -0300

    Remove scripts in examples/server

--- examples/server/memory-total.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-LIST=$(make server.ramprof)
-LIST=$(echo $LIST | sed 's/ /\\n/g')
-echo -e $LIST | tail -n$[ $(echo -e $LIST | wc -l) - 2 ] | awk '{print $1}' | awk '{tot = tot + $1}END{print tot}'
