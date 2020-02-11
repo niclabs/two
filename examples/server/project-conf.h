f5b469f414cf60c66527ead03b41a579fccf8603
Felipe Lalanne <felipe@lalanne.cl>
2020-02-11 16:34:54 -0300

    Move contiki configurations to project-conf.h

--- examples/server/project-conf.h
+++ examples/server/project-conf.h
@@ -1,14 +1,15 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* QUEUEBUF_CONF_NUM specifies the number of queue buffers. Queue
-   buffers are used throughout the Contiki netstack but the
-   configuration option can be tweaked to save memory. Performance can
-   suffer with a too low number of queue buffers though.
+/**
+ * Disable UDP
+ */
+#define UIP_CONF_UDP (0)
 
-   We reduce the queue buffer size to allow the two implementation to fit
-   in the .bss memory section. */
-#define QUEUEBUF_CONF_NUM 4
+/**
+ * Enable TCP
+ */
+#define UIP_CONF_TCP (1)
 
 /**
  * Not enough memory in contiki for multiple clients
@@ -19,5 +20,6 @@
  * Disable hpack dynamic table by default
  */
 #define CONFIG_HTTP2_HEADER_TABLE_SIZE (0)
+
 
 #endif
