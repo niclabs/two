e44a8ca50eaf56c461cc2cf951588c79fe259645
Felipe Lalanne <felipe@lalanne.cl>
2020-02-18 12:25:35 -0300

    Fix hpack utils tests

--- src/hpack/utils.h
+++ src/hpack/utils.h
@@ -3,9 +3,6 @@
 
 #include "hpack/structs.h"
 
-uint32_t hpack_utils_read_bits_from_bytes(uint16_t current_bit_pointer,
-                                          uint8_t number_of_bits_to_read,
-                                          const uint8_t *buffer);
 uint32_t hpack_utils_log128(uint32_t x);
 hpack_preamble_t hpack_utils_get_preamble(uint8_t preamble);
 uint8_t hpack_utils_find_prefix_size(hpack_preamble_t octet);
