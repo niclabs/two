2ca02649c4eecc15071cf1451a600013db97906b
Felipe Lalanne <felipe@lalanne.cl>
2020-02-25 18:19:59 -0300

    Test that http2 server receives settings

--- tests/test_http2.c
+++ tests/test_http2.c
@@ -5,6 +5,8 @@
 
 extern int waiting_for_preface(event_sock_t *client, int size, uint8_t *buf);
 extern int waiting_for_settings(event_sock_t *sock, int size, uint8_t *buf);
+extern int receiving(event_sock_t *sock, int size, uint8_t *buf);
+extern void http2_on_client_close(event_sock_t *sock);
 
 DEFINE_FFF_GLOBALS;
 
@@ -142,6 +144,35 @@ void setUp()
     FFF_RESET_HISTORY();
 }
 
+uint32_t read_u31(uint8_t *bytes)
+{
+    return ((bytes[0] & 0x7F) << 24) + (bytes[1] << 16) + (bytes[2] << 8) +
+           bytes[3];
+}
+
+uint32_t read_u24(uint8_t *bytes)
+{
+    return (bytes[0] << 16) + (bytes[1] << 8) + bytes[2];
+}
+
+uint8_t read_u8(uint8_t *bytes)
+{
+    return bytes[0];
+}
+
+void parse_header(frame_header_t *header, uint8_t *data, unsigned int size)
+{
+    (void)size;
+
+    // cleanup memory first
+    memset(header, 0, sizeof(frame_header_t));
+
+    header->length    = read_u24(data);
+    header->type      = read_u8(data + 3);
+    header->flags     = read_u8(data + 4);
+    header->stream_id = read_u31(data + 5);
+}
+
 void test_handle_good_preface(void)
 {
     event_sock_t client;
@@ -156,9 +187,103 @@ void test_handle_good_preface(void)
     TEST_ASSERT_EQUAL(1, send_settings_frame_fake.call_count);
     TEST_ASSERT_EQUAL(0, send_settings_frame_fake.arg1_val); // not an ack
 
-    // http2 goes to waitign for settings state
+    // the server goes to waiting for settings state
     TEST_ASSERT_EQUAL(1, event_read_fake.call_count);
     TEST_ASSERT_EQUAL(waiting_for_settings, event_read_fake.arg1_val);
+
+    // close client
+    http2_on_client_close(&client);
+}
+
+void test_handle_bad_preface(void)
+{
+    event_sock_t client;
+    http2_new_client(&client);
+
+    uint8_t *buf = (uint8_t *)"this is a bad preface!!!";
+
+    // the method consumes the whole buffer
+    TEST_ASSERT_EQUAL(24, waiting_for_preface(&client, 24, buf));
+
+    // on bad preface the server must close the client
+    TEST_ASSERT_EQUAL(1, event_close_fake.call_count);
+    TEST_ASSERT_EQUAL(&client, event_close_fake.arg0_val);
+    TEST_ASSERT_EQUAL(1, event_read_stop_fake.call_count);
+    TEST_ASSERT_EQUAL(&client, event_read_stop_fake.arg0_val);
+
+    // the server must not send settings
+    TEST_ASSERT_EQUAL(0, send_settings_frame_fake.call_count);
+    TEST_ASSERT_EQUAL(0, send_settings_frame_fake.arg1_val); // not an ack
+
+    // check that the server does not move to next state
+    TEST_ASSERT_EQUAL(0, event_read_fake.call_count);
+
+    // close client
+    http2_on_client_close(&client);
+}
+
+void test_receive_settings(void)
+{
+    event_sock_t client;
+    http2_context_t *ctx = http2_new_client(&client);
+
+    // use custom header parsing function
+    frame_parse_header_fake.custom_fake = parse_header;
+
+    // prepare settings frame
+    uint8_t buf[9 + 12] = { // header length 12
+                            0,
+                            0,
+                            6 + 6,
+                            // frame type settings
+                            FRAME_SETTINGS_TYPE,
+                            // no flags
+                            0,
+                            // stream id 0 (with reserved bit)
+                            0x80,
+                            0,
+                            0,
+                            0,
+                            // settings id = header table size
+                            0,
+                            0x1,
+                            // setting value = 128
+                            0,
+                            0,
+                            0,
+                            77,
+                            // settings id = initial window size
+                            0,
+                            0x4,
+                            // settings value = 122
+                            0,
+                            0,
+                            0,
+                            127
+
+    };
+
+    // the method consumed all bytes
+    TEST_ASSERT_EQUAL(21, waiting_for_settings(&client, 21, buf));
+
+    // the server updates the client context settings
+    ctx->settings.header_table_size   = 77;
+    ctx->settings.initial_window_size = 127;
+
+    // the server updates the dynamic table size
+    TEST_ASSERT_EQUAL(1, hpack_dynamic_change_max_size_fake.call_count);
+    TEST_ASSERT_EQUAL(77, hpack_dynamic_change_max_size_fake.arg1_val);
+
+    // the server replies with ack
+    TEST_ASSERT_EQUAL(1, send_settings_frame_fake.call_count);
+    TEST_ASSERT_EQUAL(1, send_settings_frame_fake.arg1_val);
+
+    // the server moved to receiving state
+    TEST_ASSERT_EQUAL(1, event_read_fake.call_count);
+    TEST_ASSERT_EQUAL(receiving, event_read_fake.arg1_val);
+
+    // close client
+    http2_on_client_close(&client);
 }
 
 int main(void)
@@ -166,5 +291,7 @@ int main(void)
 {
     UNITY_BEGIN();
     UNIT_TEST(test_handle_good_preface);
+    UNIT_TEST(test_handle_bad_preface);
+    UNIT_TEST(test_receive_settings);
     return UNITY_END();
 }
